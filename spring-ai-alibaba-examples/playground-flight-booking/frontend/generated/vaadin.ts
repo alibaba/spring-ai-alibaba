f5daaa7c466f90708cfa3d47c798a326dd7caecf
yuluo-yx <yuluo08290126@gmail.com>
2024-09-22 15:09:50 +0800

    fix
    Signed-off-by: yuluo-yx <yuluo08290126@gmail.com>

--- spring-ai-alibaba-examples/playground-flight-booking/frontend/generated/vaadin.ts
+++ spring-ai-alibaba-examples/playground-flight-booking/frontend/generated/vaadin.ts
@@ -29,26 +29,6 @@ import '@vaadin/dialog/theme/lumo/vaadin-dialog.js';
 import '@vaadin/multi-select-combo-box/theme/lumo/vaadin-multi-select-combo-box.js';
 import '@vaadin/icons/vaadin-iconset.js';
 import '@vaadin/icon/vaadin-icon.js';
-import client from 'Frontend/generated/connect-client.default.js';
-let generatedId = 0;
-const copilotMiddleware: any = async function(
-  context: any,
-  next: any
-) {
-    generatedId++;
-    const requestData = { endpoint: context.endpoint, method: context.method, params: context.params, id: generatedId };
-    (window as any).Vaadin.copilot.eventbus.emit('endpoint-request', requestData);
-
-    const response: Response = await next(context);
-    const text = await response.clone().text();
-    const responseData = { text, id: generatedId} ;
-    (window as any).Vaadin.copilot.eventbus.emit('endpoint-response', responseData);
-
-    return response;
-};
-
-client.middlewares = [...client.middlewares, copilotMiddleware];
-
 import './vaadin-featureflags.js';
 
 import './index';
@@ -59,9 +39,3 @@ import 'Frontend/generated/jar-resources/vaadin-dev-tools/vaadin-dev-tools.js';
 import './theme-customer-support-agent.global.generated.js';
 import { applyTheme } from './theme.js';
 applyTheme(document);
-
-import { Outlet } from 'react-router-dom';
-(window as any).Vaadin ??= {};
-(window as any).Vaadin.copilot ??= {};
-(window as any).Vaadin.copilot._ref ??= {};
-(window as any).Vaadin.copilot._ref.Outlet = Outlet;
