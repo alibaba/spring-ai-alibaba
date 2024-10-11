7497debb376a73df4b1d30b504acf0c4abe680f5
lwb214 <12197587+lwb214@user.noreply.gitee.com>
2024-10-11 22:49:11 +0800

    spring-ai-alibaba 使用ollama等其他大模型示例

---
+++ spring-ai-alibaba-examples/ollama-example/src/test/java/com/alibaba/cloud/ai/example/ollama/OllamaExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alibaba.cloud.ai.example.ollama;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OllamaExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
