dd68e4feab145e1d12e24f626434fa114b6f4380
yuluo-yx <yuluo08290126@gmail.com>
2024-10-18 12:00:19 +0800

    example: add multi model examples
    Signed-off-by: yuluo-yx <yuluo08290126@gmail.com>

---
+++ spring-ai-alibaba-examples/multi-model-example/src/main/java/com/alibaba/cloud/ai/example/multi/MultiModelApplication.java
@@ -0,0 +1,19 @@
+package com.alibaba.cloud.ai.example.multi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yuluo
+ * @author <a href="mailto:yuluo08290126@gmail.com">yuluo</a>
+ */
+
+@SpringBootApplication
+public class MultiModelApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(MultiModelApplication.class, args);
+	}
+
+}
