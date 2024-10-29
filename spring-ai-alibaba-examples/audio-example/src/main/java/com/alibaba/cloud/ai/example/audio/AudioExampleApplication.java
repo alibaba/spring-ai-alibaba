a3dd58602eafb294bec5acc5626ec228645ec230
yuluo-yx <yuluo08290126@gmail.com>
2024-10-29 12:06:39 +0800

    feat: add tts example
    Signed-off-by: yuluo-yx <yuluo08290126@gmail.com>

---
+++ spring-ai-alibaba-examples/audio-example/src/main/java/com/alibaba/cloud/ai/example/audio/AudioExampleApplication.java
@@ -0,0 +1,19 @@
+package com.alibaba.cloud.ai.example.audio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yuluo
+ * @author <a href="mailto:yuluo08290126@gmail.com">yuluo</a>
+ */
+
+@SpringBootApplication
+public class AudioExampleApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(AudioExampleApplication.class, args);
+	}
+
+}
---
+++ spring-ai-alibaba-examples/audio-example/src/main/java/com/alibaba/cloud/ai/example/audio/stt/STTController.java
@@ -0,0 +1,17 @@
+package com.alibaba.cloud.ai.example.audio.stt;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author yuluo
+ * @author <a href="mailto:yuluo08290126@gmail.com">yuluo</a>
+ */
+
+@RestController
+@RequestMapping("/ai/stt")
+public class STTController {
+
+	// todo: implement the controller
+
+}
