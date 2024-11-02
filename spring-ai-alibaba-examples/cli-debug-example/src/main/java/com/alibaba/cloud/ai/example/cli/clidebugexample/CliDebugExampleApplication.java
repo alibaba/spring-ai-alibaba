4d6eba67596f8b30d549767847beaecab9633ad2
conghuhu <15841721425@163.com>
2024-11-02 09:41:36 +0800

    feat: init spring-ai-alibaba-studio

---
+++ spring-ai-alibaba-examples/cli-debug-example/src/main/java/com/alibaba/cloud/ai/example/cli/clidebugexample/CliDebugExampleApplication.java
@@ -0,0 +1,13 @@
+package com.alibaba.cloud.ai.example.cli.clidebugexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CliDebugExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CliDebugExampleApplication.class, args);
+    }
+
+}
---
+++ spring-ai-alibaba-studio/src/main/java/com/alibaba/cloud/ai/api/ChatClientAPI.java
@@ -0,0 +1,8 @@
+package com.alibaba.cloud.ai.api;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "chat-client", description = "the chat-client API")
+public interface ChatClientAPI {
+
+}
