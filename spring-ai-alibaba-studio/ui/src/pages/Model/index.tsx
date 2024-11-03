491759d6e2a3ed7f7956fa298eefac98b1f460d2
SDUWYS <sdu_wys@163.com>
2024-11-03 18:25:45 +0800

    feat: chatClient demo

---
+++ spring-ai-alibaba-studio/ui/src/pages/Model/index.tsx
@@ -0,0 +1,104 @@
+import { useEffect, useState } from 'react';
+import { useRequest } from 'ice';
+import { Card, Input, Divider, Tabs, Form, Button, Select, Slider } from 'antd';
+import styles from './index.module.css';
+
+export default function Model() {
+  const { data, error, loading, request: fetchModels } = useRequest({ url: 'studio/api/chat-models' });
+  const [dataSource, setDataSource] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetchModels();
+        setDataSource(res);
+      } catch (err) {
+        console.error('Error fetching models:', err);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const onChange = (key: string) => {
+    console.log(key);
+  };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.left}>
+      {dataSource.length > 0 ? (
+        dataSource.map((model, index) => (
+          <>
+          <Card title="Model" extra={<a href="#">More</a>} style={{ width: '100%' }}>
+            <p>{model.name}</p>
+
+            <Input placeholder="Basic usage" />
+          </Card>
+          </> 
+        ))
+      ) : (
+        <h2>No Models Available</h2>
+      )}
+      </div>
+      <div className="full-height-container">
+        <Divider type="vertical" className="full-height-divider" />
+      </div>
+      <div className={styles.right}>
+      {dataSource.length > 0 ? (
+          <>
+          <Card  style={{ width: '100%' }}>
+            <Tabs
+              defaultActiveKey="1"
+              onChange={onChange}
+              items={[
+                {
+                  label: `Config`,
+                  key: '1',
+                  children: (
+                    <Form  layout="vertical">
+                      <Form.Item label="Model" name="name">
+                        <Select placeholder="Select Model" />
+                      </Form.Item>
+                      <Form.Item label="Temperature" name="temperature">
+                      <Slider defaultValue={30} disabled={false} />
+                      </Form.Item>
+                      <Form.Item label="Top_p" name="top_p">
+                      <Slider defaultValue={30} disabled={false} />
+                      </Form.Item>
+                      <Form.Item label="Top_k" name="top_k">
+                      <Slider defaultValue={30} disabled={false} />
+                      </Form.Item>
+                      <Form.Item label="Max output tokens" name="Top_k">
+                      <Input  placeholder="input tokens" />
+                      </Form.Item>
+                      <Form.Item label="Stop sequences" name="Top_k">
+                      <Input  placeholder="input sequences" />
+                      </Form.Item>
+                      <Form.Item label="Model version" name="name">
+                        <Select placeholder="Select Model version" />
+                      </Form.Item>
+                      <Form.Item>
+                        <Button type="primary" htmlType="submit">
+                          Run
+                        </Button>
+                      </Form.Item>
+                    </Form>
+                  ),
+                },
+                {
+                  label: `Tools`,
+                  key: '2',
+                  children: `Content of Tools`,
+                },
+              ]}
+            />
+          </Card>
+          </> 
+      ) : (
+        <h2>No Models Available</h2>
+      )}
+      </div>
+    </div>
+  );
+}
