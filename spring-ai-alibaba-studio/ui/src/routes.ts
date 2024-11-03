ab9b3fded31a517ee097415a5d9067e01eef69a9
jianyi-gronk <jianyi_gronk@163.com>
2024-11-03 23:26:25 +0800

    feat: 添加 Models

--- spring-ai-alibaba-studio/ui/src/routes.ts
+++ spring-ai-alibaba-studio/ui/src/routes.ts
@@ -2,7 +2,9 @@ import { IRouterConfig, lazy } from 'ice';
 import Layout from '@/Layouts/BasicLayout';
 
 const NotFound = lazy(() => import('@/components/NotFound'));
-const Model = lazy(() => import('@/pages/Model'));
+const Clients = lazy(() => import('@/pages/Clients'));
+const ChatModel = lazy(() => import('@/pages/Models/ChatModel'));
+const ImageModel = lazy(() => import('@/pages/Models/ImageModel'));
 
 const routerConfig: IRouterConfig[] = [
   {
@@ -12,12 +14,25 @@ const routerConfig: IRouterConfig[] = [
       {
         path: '/',
         exact: true,
-        component: Model,
-      }, 
+        component: Clients,
+      },
+      {
+        path: '/Models',
+        children: [
+          {
+            path: '/Models/ChatModel',
+            component: ChatModel,
+          },
+          {
+            path: '/Models/ImageModel',
+            component: ImageModel,
+          },
+        ],
+      },
       {
         component: NotFound,
-      }
-  ],
+      },
+    ],
   },
 ];
 
