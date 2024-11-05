c2201893012d59b03cc8a70cfacef094cb3ba931
conghuhu <15841721425@163.com>
2024-11-05 23:24:35 +0800

    add unify request

--- spring-ai-alibaba-studio/src/main/java/com/alibaba/cloud/ai/controller/ChatClientAPIController.java
+++ spring-ai-alibaba-studio/src/main/java/com/alibaba/cloud/ai/controller/ChatClientAPIController.java
@@ -1,9 +1,11 @@
 package com.alibaba.cloud.ai.controller;
 
 import com.alibaba.cloud.ai.api.ChatClientAPI;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 @RequestMapping("studio/api/chat-clients")
 public class ChatClientAPIController implements ChatClientAPI {
--- spring-ai-alibaba-studio/src/main/java/com/alibaba/cloud/ai/controller/ChatModelAPIController.java
+++ spring-ai-alibaba-studio/src/main/java/com/alibaba/cloud/ai/controller/ChatModelAPIController.java
@@ -3,11 +3,13 @@ package com.alibaba.cloud.ai.controller;
 import com.alibaba.cloud.ai.api.ChatModelAPI;
 import com.alibaba.cloud.ai.service.ChatModelDelegate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Optional;
 
+@CrossOrigin
 @RestController
 @RequestMapping("studio/api/chat-models")
 public class ChatModelAPIController implements ChatModelAPI {
