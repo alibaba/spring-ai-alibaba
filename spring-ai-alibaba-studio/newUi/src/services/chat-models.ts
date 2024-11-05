ef01b4037586baac0437cf20c2f2ad54c9b502cf
conghuhu <15841721425@163.com>
2024-11-06 00:04:00 +0800

    add ts type

--- spring-ai-alibaba-studio/newUi/src/services/chat-models.ts
+++ spring-ai-alibaba-studio/newUi/src/services/chat-models.ts
@@ -1,12 +1,12 @@
-import {request} from 'ice';
+import { ChatModel } from "@/types/chat_model";
+import { request } from "ice";
 
 export default {
-
-    // 获取ChatModels列表
-    async getChatModels() {
-        return await request({
-            url: 'studio/api/chat-models',
-            method: 'get',
-        });
-    },
+  // 获取ChatModels列表
+  async getChatModels(): Promise<ChatModel[]> {
+    return await request({
+      url: "studio/api/chat-models",
+      method: "get",
+    });
+  },
 };
