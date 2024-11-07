ee164d91bbb8d1ffadd4de46ffe862a7f54576d7
cirilla-zmh <zmh1625@163.com>
2024-11-07 10:51:08 +0800

    feat: add DashScopeChatModel call and stream observability

---
+++ spring-ai-alibaba-core/src/main/java/com/alibaba/cloud/ai/observation/conventions/AiProvider.java
@@ -0,0 +1,33 @@
+package com.alibaba.cloud.ai.observation.conventions;
+
+/**
+ * Extended collection of systems providing AI functionality. Based on the OpenTelemetry
+ * Semantic Conventions for AI Systems.
+ *
+ * @author Lumian
+ * @since 1.0.0
+ * @see <a href=
+ * "https://github.com/open-telemetry/semantic-conventions/tree/main/docs/gen-ai">OTel
+ * Semantic Conventions</a>.
+ * @see org.springframework.ai.observation.conventions.AiProvider
+ */
+public enum AiProvider {
+
+    // @formatter:off
+
+    // Please, keep the alphabetical sorting.
+    DASHSCOPE("dashscope");
+
+    private final String value;
+
+    AiProvider(String value) {
+        this.value = value;
+    }
+
+    public String value() {
+        return this.value;
+    }
+
+    // @formatter:on
+
+}
