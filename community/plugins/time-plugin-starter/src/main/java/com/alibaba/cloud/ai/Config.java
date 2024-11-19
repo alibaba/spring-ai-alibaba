45ecab7e312810af1054563baf834a578c601d3d
chengle <chengle.lz@alibaba-inc.com>
2024-11-19 20:10:35 +0800

    add the function of getting a city time

--- community/plugins/time-plugin-starter/src/main/java/com/alibaba/cloud/ai/Config.java
+++ community/plugins/time-plugin-starter/src/main/java/com/alibaba/cloud/ai/Config.java
@@ -1,6 +1,7 @@
 package com.alibaba.cloud.ai;
 
 import com.alibaba.cloud.ai.service.GetCurrentLocalTimeService;
+import com.alibaba.cloud.ai.service.GetCurrentTimeByTimeZoneIdService;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
@@ -14,5 +15,12 @@ public class Config {
     @Description("Get the current local time")
     public GetCurrentLocalTimeService getCurrentLocalTimeFunction() {
         return new GetCurrentLocalTimeService();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    @Description("Get the current time based on time zone id")
+    public GetCurrentTimeByTimeZoneIdService getCityTimeFunction() {
+        return new GetCurrentTimeByTimeZoneIdService();
     }
 }
