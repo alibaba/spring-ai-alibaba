fb7c6bfb6f4bc71db2eef745a0e1472802b03cc3
jianchuan.zjc <jianchuan.zjc@alibaba-inc.com>
2024-11-20 12:21:35 +0800

    优化内容解析

--- community/baidu-search-plugin-starter/src/test/java/com/alibaba/cloud/plugin/BaiduSearchServiceTest.java
+++ community/baidu-search-plugin-starter/src/test/java/com/alibaba/cloud/plugin/BaiduSearchServiceTest.java
@@ -8,27 +8,26 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class BaiduSearchServiceTest {
-
-	private BaiduSearchService baiduSearchService;
-
-	@BeforeEach
-	public void setUp() {
-		baiduSearchService = new BaiduSearchService();
-	}
-
-	@Test
-	public void testSearch() {
-		BaiduSearchService.Request request = new BaiduSearchService.Request("阿里巴巴上市时间", 10);
-		BaiduSearchService.Response response = baiduSearchService.apply(request);
-
-		assertNotNull(response);
-		assertFalse(response.results().isEmpty());
-		assertEquals(10, response.results().size());
-
-		for (BaiduSearchService.SearchResult result : response.results()) {
-			assertNotNull(result.title());
-			assertNotNull(result.abstractText());
-		}
-	}
-
+    
+    private BaiduSearchService baiduSearchService;
+    
+    @BeforeEach
+    public void setUp() {
+        baiduSearchService = new BaiduSearchService();
+    }
+    
+    @Test
+    public void testSearch() {
+        BaiduSearchService.Request request = new BaiduSearchService.Request("阿里巴巴上市时间", 10);
+        BaiduSearchService.Response response = baiduSearchService.apply(request);
+        
+        assertNotNull(response);
+        assertFalse(response.results().isEmpty());
+        assertEquals(10, response.results().size());
+        
+        for (BaiduSearchService.SearchResult result : response.results()) {
+            assertNotNull(result.title());
+            assertNotNull(result.abstractText());
+        }
+    }
 }
