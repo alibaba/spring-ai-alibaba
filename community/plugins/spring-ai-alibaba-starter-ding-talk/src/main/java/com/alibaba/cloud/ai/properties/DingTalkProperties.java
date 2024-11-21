fa49a77ff07fefb67a674d5ce351369645ed2c8c
YunLong <jiangyunlong@chongho.net>
2024-11-21 14:22:51 +0800

    1.add ding-talk properties

---
+++ community/plugins/spring-ai-alibaba-starter-ding-talk/src/main/java/com/alibaba/cloud/ai/properties/DingTalkProperties.java
@@ -0,0 +1,51 @@
+package com.alibaba.cloud.ai.properties;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author YunLong
+ */
+@ConfigurationProperties("spring.ai.alibaba.plugin.ding-talk")
+public class DingTalkProperties {
+
+    private CustomRobot customRobot;
+
+    public DingTalkProperties() {
+    }
+
+    public DingTalkProperties(CustomRobot customRobot) {
+        this.customRobot = customRobot;
+    }
+
+    public CustomRobot getCustomRobot() {
+        return customRobot;
+    }
+
+    public void setCustomRobot(CustomRobot customRobot) {
+        this.customRobot = customRobot;
+    }
+
+    public static class CustomRobot {
+
+        private String accessToken;
+
+        private String signature;
+
+        public String getAccessToken() {
+            return accessToken;
+        }
+
+        public void setAccessToken(String accessToken) {
+            this.accessToken = accessToken;
+        }
+
+        public String getSignature() {
+            return signature;
+        }
+
+        public void setSignature(String signature) {
+            this.signature = signature;
+        }
+    }
+}
