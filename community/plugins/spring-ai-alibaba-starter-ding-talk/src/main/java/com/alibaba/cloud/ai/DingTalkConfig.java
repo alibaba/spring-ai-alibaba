265a3886d9f911c159f8ad79a149340810126e97
YunLong <jiangyunlong@chongho.net>
2024-11-21 14:22:51 +0800

    1.add ding-talk properties

---
+++ community/plugins/spring-ai-alibaba-starter-ding-talk/src/main/java/com/alibaba/cloud/ai/DingTalkConfig.java
@@ -0,0 +1,24 @@
+package com.alibaba.cloud.ai;
+
+import com.alibaba.cloud.ai.properties.DingTalkProperties;
+import com.alibaba.cloud.ai.service.CustomRobotSendMessageService;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Description;
+
+/**
+ * @author YunLong
+ */
+@EnableConfigurationProperties(DingTalkProperties.class)
+@ConditionalOnProperty(prefix = "spring.ai.alibaba.plugin.ding-talk", name = "enabled", havingValue = "true")
+public class DingTalkConfig {
+
+    @Bean
+    @ConditionalOnMissingBean
+    @Description("Send group chat messages using a custom robot")
+    public CustomRobotSendMessageService CustomRobotSendMessageFunction(DingTalkProperties dingTalkProperties) {
+        return new CustomRobotSendMessageService(dingTalkProperties);
+    }
+}
