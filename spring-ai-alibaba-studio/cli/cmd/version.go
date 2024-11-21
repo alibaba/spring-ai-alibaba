6409886611f6064148bcfdc9d895002baae03276
wangguan <wg1024code@gmail.com>
2024-11-21 10:42:35 +0800

    fix(cli): version help message

--- spring-ai-alibaba-studio/cli/cmd/version.go
+++ spring-ai-alibaba-studio/cli/cmd/version.go
@@ -24,13 +24,7 @@ import (
 // versionCmd represents the version command
 var versionCmd = &cobra.Command{
 	Use:   "version",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Print the version number of spring-ai-alibaba",
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println(constant.Version)
 	},
