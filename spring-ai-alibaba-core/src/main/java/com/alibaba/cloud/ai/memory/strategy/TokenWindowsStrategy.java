8fc8f63f96ba8849a0004466e71c3d409439601f
yuluo-yx <yuluo08290126@gmail.com>
2024-11-21 21:31:49 +0800

    feat: spring ai alibaba chat memory implements
    Signed-off-by: yuluo-yx <yuluo08290126@gmail.com>

---
+++ spring-ai-alibaba-core/src/main/java/com/alibaba/cloud/ai/memory/strategy/TokenWindowsStrategy.java
@@ -0,0 +1,36 @@
+package com.alibaba.cloud.ai.memory.strategy;
+
+import com.alibaba.cloud.ai.dashscope.api.DashScopeApi;
+
+/**
+ * @author yuluo
+ * @author <a href="mailto:yuluo08290126@gmail.com">yuluo</a>
+ */
+public class TokenWindowsStrategy extends AbstractChatMemoryStrategy {
+
+	private final String id;
+    private final Integer maxTokens;
+    private final DashScopeApi.TokenUsage tokenUsage;
+
+	/**
+	 * 使用 build
+	 * @param id
+	 * @param maxTokens
+	 * @param tokenUsage
+	 */
+	public TokenWindowsStrategy(String id, Integer maxTokens, DashScopeApi.TokenUsage tokenUsage) {
+		this.id = id;
+		this.maxTokens = maxTokens;
+		this.tokenUsage = tokenUsage;
+	}
+
+	@Override
+	public void ensureCapacity() {
+
+		// 超出淘汰
+		if (this.maxTokens > DEFAULT_CAPACITY) {
+			disuse();
+		}
+	}
+
+}
