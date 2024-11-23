f2405d354da0d035250d7d68a10d23190e4535cc
PolarishT <zhangzhenting@corp.netease.com>
2024-11-23 21:06:37 +0800

    fix format pom dependencies & add license
    Signed-off-by: PolarishT <zhangzhenting@corp.netease.com>

--- community/plugins/spring-ai-alibaba-plugin-larksuite-starter/src/main/java/com/alibaba/cloud/ai/service/LarkSuiteService.java
+++ community/plugins/spring-ai-alibaba-plugin-larksuite-starter/src/main/java/com/alibaba/cloud/ai/service/LarkSuiteService.java
@@ -1,4 +1,3 @@
-package com.alibaba.cloud.ai.service;
 /*
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
@@ -15,6 +14,7 @@ package com.alibaba.cloud.ai.service;
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.alibaba.cloud.ai.service;
 
 import com.alibaba.cloud.ai.properties.LarkSuiteProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
