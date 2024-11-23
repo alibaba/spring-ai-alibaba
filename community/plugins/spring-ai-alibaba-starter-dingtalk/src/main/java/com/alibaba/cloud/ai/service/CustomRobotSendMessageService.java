c0f3f635b78c5596628ef064cb2511b962bfb78d
YunLong <jiangyunlong@chongho.net>
2024-11-23 10:45:19 +0800

    1.add license header

--- community/plugins/spring-ai-alibaba-starter-dingtalk/src/main/java/com/alibaba/cloud/ai/service/CustomRobotSendMessageService.java
+++ community/plugins/spring-ai-alibaba-starter-dingtalk/src/main/java/com/alibaba/cloud/ai/service/CustomRobotSendMessageService.java
@@ -1,5 +1,20 @@
 package com.alibaba.cloud.ai.service;
-
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 import com.alibaba.cloud.ai.properties.DingTalkProperties;
 import com.alibaba.cloud.ai.utils.SignUtils;
 import com.dingtalk.api.DefaultDingTalkClient;
@@ -31,12 +46,12 @@ public class CustomRobotSendMessageService implements Function<CustomRobotSendMe
     @Override
     public Response apply(Request request) {
 
-        if (ObjectUtils.isEmpty(dingTalkProperties.getCustomRobot().getAccessToken()) || ObjectUtils.isEmpty(dingTalkProperties.getCustomRobot().getSignature())) {
+        if (ObjectUtils.isEmpty(dingTalkProperties.getCustomRobotAccessToken()) || ObjectUtils.isEmpty(dingTalkProperties.getCustomRobotSignature())) {
             throw new IllegalArgumentException("current spring.ai.community.plugin.dingTalk.customRobot must not be null.");
         }
 
-        String accessToken = dingTalkProperties.getCustomRobot().getAccessToken();
-        String signature = dingTalkProperties.getCustomRobot().getSignature();
+        String accessToken = dingTalkProperties.getCustomRobotAccessToken();
+        String signature = dingTalkProperties.getCustomRobotSignature();
 
         // Request Body, please see the official document for more parameters.
         OapiRobotSendRequest req = new OapiRobotSendRequest();
