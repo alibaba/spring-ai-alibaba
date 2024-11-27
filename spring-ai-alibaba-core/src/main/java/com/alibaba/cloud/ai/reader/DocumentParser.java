8d5f289374245e3e0b2fc2b1ae650639c4d2a4da
chickenlj <ken.lj.hz@gmail.com>
2024-11-27 15:50:40 +0800

    add more parser type configuration

--- spring-ai-alibaba-core/src/main/java/com/alibaba/cloud/ai/reader/DocumentParser.java
+++ spring-ai-alibaba-core/src/main/java/com/alibaba/cloud/ai/reader/DocumentParser.java
@@ -24,24 +24,31 @@ import org.springframework.core.io.Resource;
 
 public enum DocumentParser {
 
-	TEXT_PARSER("text", "org.springframework.ai.reader.TextReader"),
-	JSON_PARSER("json", "org.springframework.ai.reader.JsonReader"),
-	PAGE_PDF_PARSER("pagePdf", "org.springframework.ai.reader.pdf.PagePdfDocumentReader"),
-	PARAGRAPH_PDF_PARSER("paragraphPdf", "org.springframework.ai.reader.pdf.ParagraphPdfDocumentReader"),
-	DOCX_PARSER("docx", "com.alibaba.cloud.ai.reader.poi.PoiDocumentReader"),
-	DOC_PARSER("doc", "com.alibaba.cloud.ai.reader.poi.PoiDocumentReader"),
-	PPTX_PARSER("pptx", "com.alibaba.cloud.ai.reader.poi.PoiDocumentReader"),
-	PPT_PARSER("ppt", "com.alibaba.cloud.ai.reader.poi.PoiDocumentReader"),
-	XLSX_PARSER("xlsx", "com.alibaba.cloud.ai.reader.poi.PoiDocumentReader"),
-	XLS_PARSER("xls", "com.alibaba.cloud.ai.reader.poi.PoiDocumentReader");
+	TEXT_PARSER("text", "org.springframework.ai.reader.TextReader", "Plain Text Document Reader"),
+	JSON_PARSER("json", "org.springframework.ai.reader.JsonReader", "Plain Json Document Reader"),
+	PAGE_PDF_PARSER("pagePdf", "org.springframework.ai.reader.pdf.PagePdfDocumentReader", "Groups the parsed PDF pages into {@link Document}s. You can group one or more pages into a single output document."),
+	PARAGRAPH_PDF_PARSER("paragraphPdf", "org.springframework.ai.reader.pdf.ParagraphPdfDocumentReader", "Uses the PDF catalog (e.g. TOC) information to split the input PDF into text paragraphs and output a single {@link Document} per paragraph"),
+	DOCX_PARSER("docx", "com.alibaba.cloud.ai.reader.poi.PoiDocumentReader", "Parses Microsoft Office file into a {@link Document} using Apache POI library"),
+	DOC_PARSER("doc", "com.alibaba.cloud.ai.reader.poi.PoiDocumentReader", "Parses Microsoft Office file into a {@link Document} using Apache POI library"),
+	PPTX_PARSER("pptx", "com.alibaba.cloud.ai.reader.poi.PoiDocumentReader", "Parses Microsoft Office file into a {@link Document} using Apache POI library"),
+	PPT_PARSER("ppt", "com.alibaba.cloud.ai.reader.poi.PoiDocumentReader", "Parses Microsoft Office file into a {@link Document} using Apache POI library"),
+	XLSX_PARSER("xlsx", "com.alibaba.cloud.ai.reader.poi.PoiDocumentReader", "Parses Microsoft Office file into a {@link Document} using Apache POI library"),
+	XLS_PARSER("xls", "com.alibaba.cloud.ai.reader.poi.PoiDocumentReader", "Parses Microsoft Office file into a {@link Document} using Apache POI library"),
+	CSV_PARSER("csv", "org.springframework.ai.reader.tika.TikaDocumentReader", "Uses tika to extract text content from CSV documents."),
+	HTML_PARSER("html", "org.springframework.ai.reader.tika.TikaDocumentReader", "Uses tika to extract text content from HTML documents."),
+	XML_PARSER("xml", "org.springframework.ai.reader.tika.TikaDocumentReader", "Uses tika to extract text content from XML documents."),
+	RTF_PARSER("rtf", "org.springframework.ai.reader.tika.TikaDocumentReader", "Uses tika to extract text content from Rich Text Format (RTF) documents.");
 
 	private final String parserType;
 
+	private final String description;
+
 	private final String parserClass;
 
-	DocumentParser(String parserType, String parserClass) {
+	DocumentParser(String parserType, String parserClass, String description) {
 		this.parserType = parserType;
 		this.parserClass = parserClass;
+		this.description = description;
 	}
 
 	public DocumentReader getParser(Resource resource) {
