15da9d046a318e4221cbf2ba872b4378bf25fee3
31445 <3144550951@qq.com>
2024-11-28 15:54:21 +0800

    update pom.xml

--- community/plugins/spring-ai-alibaba-starter-plugin-translate/src/main/java/com/alibaba/cloud/ai/plugin/translate/TranslateService.java
+++ community/plugins/spring-ai-alibaba-starter-plugin-translate/src/main/java/com/alibaba/cloud/ai/plugin/translate/TranslateService.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.alibaba.cloud.ai.plugin.translate;
 
 import com.fasterxml.jackson.annotation.JsonClassDescription;
@@ -88,10 +105,8 @@ public class TranslateService implements Function<TranslateService.Request, Tran
 
     @JsonClassDescription("Request to translate text to a target language")
     public record Request(
-            @JsonProperty(required = true, value = "text")
-            @JsonPropertyDescription("Content that needs to be translated") String text,
-            @JsonProperty(required = true, value = "targetLanguage")
-            @JsonPropertyDescription("Target language to translate into") String targetLanguage) {
+            @JsonProperty(required = true, value = "text") @JsonPropertyDescription("Content that needs to be translated") String text,
+            @JsonProperty(required = true, value = "targetLanguage") @JsonPropertyDescription("Target language to translate into") String targetLanguage) {
     }
 
     @JsonClassDescription("Response to translate text to a target language")
