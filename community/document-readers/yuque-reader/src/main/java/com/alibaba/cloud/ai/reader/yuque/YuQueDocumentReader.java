27278b8cd8de1e6256ae0f4cc40e00bf7e5f0afc
YunLong <jiangyunlong@chongho.net>
2024-11-29 17:40:08 +0800

    1.init yuquereader

---
+++ community/document-readers/yuque-reader/src/main/java/com/alibaba/cloud/ai/reader/yuque/YuQueDocumentReader.java
@@ -0,0 +1,41 @@
+package com.alibaba.cloud.ai.reader.yuque;
+
+import com.alibaba.cloud.ai.reader.DocumentParser;
+import org.springframework.ai.document.Document;
+import org.springframework.ai.document.DocumentReader;
+import org.springframework.ai.reader.ExtractedTextFormatter;
+
+import java.util.List;
+
+/**
+ * @author YunLong
+ */
+public class YuQueDocumentReader implements DocumentReader {
+
+    private DocumentReader parser;
+
+    private final YuQueResource yuQueResource;
+
+    public YuQueDocumentReader(YuQueResource yuQueResource, DocumentParser parserType) {
+        this(yuQueResource, parserType.getParser(yuQueResource));
+    }
+
+    public YuQueDocumentReader(YuQueResource yuQueResource, DocumentParser parserType, ExtractedTextFormatter formatter) {
+        this(yuQueResource, parserType.getParser(yuQueResource, formatter));
+    }
+
+    public YuQueDocumentReader(YuQueResource yuQueResource, DocumentReader parser) {
+        this.yuQueResource = yuQueResource;
+        this.parser = parser;
+    }
+
+    @Override
+    public List<Document> get() {
+        List<Document> documents = parser.get();
+
+        return documents;
+    }
+
+
+
+}
