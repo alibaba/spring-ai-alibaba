79b76d5399129689d6065412d1309ce6373d504f
liuzhifei <2679431923@qq.com>
2024-12-01 14:29:51 +0800

    run spring format and add traceDetail init comp

--- spring-ai-alibaba-graph/spring-ai-alibaba-graph-core/src/test/java/com/alibaba/cloud/ai/graph/StateGraphMongoPersistenceTest.java
+++ spring-ai-alibaba-graph/spring-ai-alibaba-graph-core/src/test/java/com/alibaba/cloud/ai/graph/StateGraphMongoPersistenceTest.java
@@ -11,34 +11,39 @@ import java.util.Map;
 import java.util.Optional;
 
 public class StateGraphMongoPersistenceTest {
-    @Test
-    public void testMongoPersistenceApi() throws Exception {
-        MongoClient client = MongoClients.create("mongodb://host:port");
-        MongoSaver saver = new MongoSaver(client);
-        RunnableConfig runnableConfig = RunnableConfig.builder().checkPointId("7faca71d-4014-44c8-ad07-fab5d5d9bb07").threadId("test-thread-3").build();
-        Map<String, Object> message = Map.of("message", "hello world");
-        Checkpoint checkPoint = Checkpoint.builder()
-                .nodeId("agent_1")
-                .state(message)
-                .nextNodeId(StateGraph.END)
-                .build();
 
-        //put
-        RunnableConfig put = saver.put(runnableConfig, checkPoint);
-        System.out.println("put = " + put);
+	@Test
+	public void testMongoPersistenceApi() throws Exception {
+		MongoClient client = MongoClients.create("mongodb://host:port");
+		MongoSaver saver = new MongoSaver(client);
+		RunnableConfig runnableConfig = RunnableConfig.builder()
+			.checkPointId("7faca71d-4014-44c8-ad07-fab5d5d9bb07")
+			.threadId("test-thread-3")
+			.build();
+		Map<String, Object> message = Map.of("message", "hello world");
+		Checkpoint checkPoint = Checkpoint.builder()
+			.nodeId("agent_1")
+			.state(message)
+			.nextNodeId(StateGraph.END)
+			.build();
 
-        //get
-        Optional<Checkpoint> checkpoint = saver.get(runnableConfig);
-        System.out.println("checkpoint = " + checkpoint);
+		// put
+		RunnableConfig put = saver.put(runnableConfig, checkPoint);
+		System.out.println("put = " + put);
 
-        //list
-        Collection<Checkpoint> list = saver.list(runnableConfig);
-        System.out.println("list = " + list);
+		// get
+		Optional<Checkpoint> checkpoint = saver.get(runnableConfig);
+		System.out.println("checkpoint = " + checkpoint);
 
-        //clear
-        boolean clear = saver.clear(runnableConfig.withCheckPointId(checkPoint.getId()));
-        System.out.println("clear = " + clear);
-        Collection<Checkpoint> list1 = saver.list(runnableConfig);
-        System.out.println("list1 = " + list1);
-    }
+		// list
+		Collection<Checkpoint> list = saver.list(runnableConfig);
+		System.out.println("list = " + list);
+
+		// clear
+		boolean clear = saver.clear(runnableConfig.withCheckPointId(checkPoint.getId()));
+		System.out.println("clear = " + clear);
+		Collection<Checkpoint> list1 = saver.list(runnableConfig);
+		System.out.println("list1 = " + list1);
+	}
+
 }
