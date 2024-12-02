e3b3c4709efdecff6b15f619dd07cbbdadc3da8f
wblu214 <wblu.eric@gmail.com>
2024-12-02 22:52:35 +0800

    add feishu reader to SAA

---
+++ community/document-readers/feishu-document-reader/src/test/java/com/alibaba/cloud/ai/reader/feishu/SpringAiAlibabaStarterPluginFeishuApplicationTests.java
@@ -0,0 +1,16 @@
+package com.alibaba.cloud.ai.reader.feishu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+/**
+ * @author wblu214
+ * @author <a href="mailto:2897718178@qq.com">wblu214</a>
+ */
+@SpringBootTest
+class SpringAiAlibabaStarterPluginFeiShuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
