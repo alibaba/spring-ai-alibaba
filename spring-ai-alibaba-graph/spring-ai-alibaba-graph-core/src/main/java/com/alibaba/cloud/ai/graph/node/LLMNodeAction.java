4fb9b77ac31955619da854f2542c56aa14891edc
wxbty <wxb_101@163.com>
2024-12-11 13:58:15 +0800

    修复构建时格式错误提示

--- spring-ai-alibaba-graph/spring-ai-alibaba-graph-core/src/main/java/com/alibaba/cloud/ai/graph/node/LLMNodeAction.java
+++ spring-ai-alibaba-graph/spring-ai-alibaba-graph-core/src/main/java/com/alibaba/cloud/ai/graph/node/LLMNodeAction.java
@@ -34,66 +34,75 @@ import java.util.stream.Collectors;
  */
 public class LLMNodeAction<State extends AgentState> extends AbstractNode implements NodeAction<State> {
 
-    /**
-     * each llm node has their own state
-     */
-    public static final String MESSAGES_KEY = "messages";
+	/**
+	 * each llm node has their own state
+	 */
+	public static final String MESSAGES_KEY = "messages";
 
-    public ChatClient chatClient;
+	public ChatClient chatClient;
 
-    public LLMNodeAction (ChatClient chatClient) {
-        this.chatClient = chatClient;
-    }
+	public LLMNodeAction(ChatClient chatClient) {
+		this.chatClient = chatClient;
+	}
 
-    public static Builder builder (ChatModel chatModel) {
+	public static Builder builder(ChatModel chatModel) {
 
-        return new Builder(chatModel);
-    }
+		return new Builder(chatModel);
+	}
 
-    /**
-     * @param state
-     * @return Map<String, Object>
-     * @throws Exception
-     */
-    @Override
-    public Map<String, Object> apply (State state) throws Exception {
+	/**
+	 * @param state
+	 * @return Map<String, Object>
+	 * @throws Exception
+	 */
+	@Override
+	public Map<String, Object> apply(State state) throws Exception {
 
-        List<Message> messages = state.value(MESSAGES_KEY, new ArrayList<>());
-        List<Generation> generations = chatClient.prompt().user(s -> s.params(state.data())).messages(messages).call().chatResponse().getResults();
-        List<Message> output = generations.stream().map(Generation::getOutput).collect(Collectors.toList());
-        return Map.of(MESSAGES_KEY, output);
-    }
+		List<Message> messages = state.value(MESSAGES_KEY, new ArrayList<>());
+		List<Generation> generations = chatClient.prompt()
+			.user(s -> s.params(state.data()))
+			.messages(messages)
+			.call()
+			.chatResponse()
+			.getResults();
+		List<Message> output = generations.stream().map(Generation::getOutput).collect(Collectors.toList());
+		return Map.of(MESSAGES_KEY, output);
+	}
 
-    public static class Builder {
+	public static class Builder {
 
-        protected ChatModel chatModel;
+		protected ChatModel chatModel;
 
-        protected String sysPrompt;
+		protected String sysPrompt;
 
-        protected String[] functions;
+		protected String[] functions;
 
-        public Builder (ChatModel chatModel) {
-            this.chatModel = chatModel;
-        }
+		public Builder(ChatModel chatModel) {
+			this.chatModel = chatModel;
+		}
 
-        public <State extends AgentState> LLMNodeAction<AgentState> build () {
-            ChatClient.Builder builder = ChatClient.builder(this.chatModel);
-            String sysPr = Optional.ofNullable(sysPrompt).orElse("{'role': 'system', 'content': 'You are a helpful assistant.'}");
-            builder.defaultSystem(sysPr);
-            if (functions != null && functions.length > 0) builder.defaultFunctions(functions);
-            return new LLMNodeAction<>(builder.build());
-        }
+		public <State extends AgentState> LLMNodeAction<AgentState> build() {
+			ChatClient.Builder builder = ChatClient.builder(this.chatModel);
+			String sysPr = Optional.ofNullable(sysPrompt)
+				.orElse("{'role': 'system', 'content': 'You are a helpful assistant.'}");
+			builder.defaultSystem(sysPr);
+			if (functions != null && functions.length > 0)
+				builder.defaultFunctions(functions);
+			return new LLMNodeAction<>(builder.build());
+		}
 
-        public Builder withSysPrompt (String prompt) {
-            this.sysPrompt = prompt;
-            return this;
-        }
+		public Builder withSysPrompt(String prompt) {
+			this.sysPrompt = prompt;
+			return this;
+		}
 
-        public Builder withFunctions (String... functionNames) {
-            if (functionNames == null || functionNames.length == 0) return this;
-            this.functions = functionNames;
-            return this;
-        }
-    }
+		public Builder withFunctions(String... functionNames) {
+			if (functionNames == null || functionNames.length == 0)
+				return this;
+			this.functions = functionNames;
+			return this;
+		}
+
+	}
+
 }
-
