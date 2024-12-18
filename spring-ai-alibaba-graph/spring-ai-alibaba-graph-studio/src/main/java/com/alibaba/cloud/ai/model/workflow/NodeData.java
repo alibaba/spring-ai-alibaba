80146bd1261ffb8a4346659cab4f179468b85932
Disaster <58838568+disaster1-tesk@users.noreply.github.com>
2024-12-18 21:41:55 +0800

    :busts_in_silhouette: Opt impl
    convert logic opt

--- spring-ai-alibaba-graph/spring-ai-alibaba-graph-studio/src/main/java/com/alibaba/cloud/ai/model/workflow/NodeData.java
+++ spring-ai-alibaba-graph/spring-ai-alibaba-graph-studio/src/main/java/com/alibaba/cloud/ai/model/workflow/NodeData.java
@@ -17,22 +17,6 @@ import java.util.List;
 @NoArgsConstructor
 public class NodeData {
     /**
-     * type of the node
-     */
-    private String type;
-    /**
-     * title of the node
-     */
-    private String title;
-    /**
-     * description of the node
-     */
-    private String desc;
-    /**
-     * whether the node is selected
-     */
-    private boolean selected;
-    /**
      * The inputs of the node is the output reference of the previous node
      */
     protected List<VariableSelector> inputs;
@@ -42,14 +26,4 @@ public class NodeData {
      */
     protected List<Variable> outputs;
 
-    /**
-     * Instantiates a new Node data.
-     *
-     * @param inputs  the inputs
-     * @param outputs the outputs
-     */
-    public NodeData(List<VariableSelector> inputs, List<Variable> outputs) {
-        this.inputs = inputs;
-        this.outputs = outputs;
-    }
 }
