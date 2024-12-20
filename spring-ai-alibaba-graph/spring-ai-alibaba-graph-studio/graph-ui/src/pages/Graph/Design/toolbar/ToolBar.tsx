fe2909c74c6271f4272fb196f1804cb8accff6b2
gronk <jianyi_gronk@163.com>
2024-12-21 01:39:12 +0800

    feat: 添加历史工具栏

--- spring-ai-alibaba-graph/spring-ai-alibaba-graph-studio/graph-ui/src/pages/Graph/Design/toolbar/ToolBar.tsx
+++ spring-ai-alibaba-graph/spring-ai-alibaba-graph-studio/graph-ui/src/pages/Graph/Design/toolbar/ToolBar.tsx
@@ -5,11 +5,21 @@ import './toolbar.less';
 
 interface Props {
   name?: string;
+  handleUndo?: () => void;
+  handleRedo?: () => void;
+  history?: [];
 }
 
-const ToolBar: React.FC<Props> = () => {
+const ToolBar: React.FC<Props> = (props) => {
+  const { handleUndo, handleRedo } = props;
   const { zoomIn, zoomOut, zoomTo, fitView } = useReactFlow();
   const { zoom } = useViewport();
+
+  const buttonsDisabled = {
+    undo: !Boolean(handleUndo),
+    redo: !Boolean(handleRedo),
+    history: Boolean(history && history.length),
+  };
 
   return (
     <div className="toolbar">
@@ -32,6 +42,39 @@ const ToolBar: React.FC<Props> = () => {
           }}
         >
           <Icon className="icon" icon="iconamoon:zoom-in-light"></Icon>
+        </div>
+      </div>
+      <div className="undo-redo-wrapper">
+        <div
+          className="icon-wrapper"
+          onClick={() => !buttonsDisabled.undo && handleUndo && handleUndo()}
+        >
+          <Icon
+            className="icon"
+            icon="iconamoon:do-redo-light"
+            style={{ color: buttonsDisabled.undo ? '#d0d5dc' : '#676f83' }}
+          ></Icon>
+        </div>
+        <div
+          className="icon-wrapper"
+          onClick={() => !buttonsDisabled.redo && handleRedo && handleRedo()}
+        >
+          <Icon
+            className="icon"
+            icon="iconamoon:do-undo-light"
+            style={{ color: buttonsDisabled.redo ? '#d0d5dc' : '#676f83' }}
+          ></Icon>
+        </div>
+        <div className="split"></div>
+        <div
+          className="icon-wrapper"
+          onClick={() => !buttonsDisabled.history && handleRedo && handleRedo()}
+        >
+          <Icon
+            className="icon"
+            icon="iconamoon:history-light"
+            style={{ color: buttonsDisabled.history ? '#d0d5dc' : '#676f83' }}
+          ></Icon>
         </div>
       </div>
     </div>
