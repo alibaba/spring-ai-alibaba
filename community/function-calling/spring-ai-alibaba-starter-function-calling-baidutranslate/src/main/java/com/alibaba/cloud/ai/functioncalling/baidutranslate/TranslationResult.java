afb8ea0f469dcfc42d177d178affd5dfff9184d8
SCMRCORE <3551147139@qq.com>
2024-12-21 15:29:12 +0800

    feat:add baidu translate impl

---
+++ community/function-calling/spring-ai-alibaba-starter-function-calling-baidutranslate/src/main/java/com/alibaba/cloud/ai/functioncalling/baidutranslate/TranslationResult.java
@@ -0,0 +1,25 @@
+package com.alibaba.cloud.ai.functioncalling.baidutranslate;
+
+class TranslationResult {
+
+	private String src;
+
+	private String dst;
+
+	public String getSrc() {
+		return src;
+	}
+
+	public void setSrc(String src) {
+		this.src = src;
+	}
+
+	public String getDst() {
+		return dst;
+	}
+
+	public void setDst(String dst) {
+		this.dst = dst;
+	}
+
+}
