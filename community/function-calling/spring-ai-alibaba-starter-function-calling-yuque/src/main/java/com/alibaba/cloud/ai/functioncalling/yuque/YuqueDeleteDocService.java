9d3ee7afdd6316484d0fb43c0163553875bdb0f5
chickenlj <ken.lj.hz@gmail.com>
2024-12-25 17:04:31 +0800

    code format

--- community/function-calling/spring-ai-alibaba-starter-function-calling-yuque/src/main/java/com/alibaba/cloud/ai/functioncalling/yuque/YuqueDeleteDocService.java
+++ community/function-calling/spring-ai-alibaba-starter-function-calling-yuque/src/main/java/com/alibaba/cloud/ai/functioncalling/yuque/YuqueDeleteDocService.java
@@ -27,42 +27,43 @@ import static com.alibaba.cloud.ai.functioncalling.yuque.YuqueProperties.BASE_UR
 /**
  * @author 北极星
  */
-public class YuqueDeleteDocService implements Function<YuqueDeleteDocService.deleteDocRequest, YuqueDeleteDocService.deleteDocResponse> {
+public class YuqueDeleteDocService
+		implements Function<YuqueDeleteDocService.deleteDocRequest, YuqueDeleteDocService.deleteDocResponse> {
 
-    private final WebClient webClient;
+	private final WebClient webClient;
 
-    public YuqueDeleteDocService (YuqueProperties yuqueProperties) {
-        this.webClient = WebClient.builder().baseUrl(BASE_URL).defaultHeader("X-Auth-Token", yuqueProperties.getAuthToken()).build();
+	public YuqueDeleteDocService(YuqueProperties yuqueProperties) {
+		this.webClient = WebClient.builder()
+			.baseUrl(BASE_URL)
+			.defaultHeader("X-Auth-Token", yuqueProperties.getAuthToken())
+			.build();
 
-    }
+	}
 
-    @Override
-    public YuqueDeleteDocService.deleteDocResponse apply (YuqueDeleteDocService.deleteDocRequest deleteDocRequest) {
-        Mono<YuqueDeleteDocService.deleteDocResponse> deleteDocResponseMono = webClient.method(HttpMethod.DELETE).uri("/repos/{book_id}/docs/{id}", deleteDocRequest.bookId, deleteDocRequest.id).retrieve().bodyToMono(YuqueDeleteDocService.deleteDocResponse.class);
+	@Override
+	public YuqueDeleteDocService.deleteDocResponse apply(YuqueDeleteDocService.deleteDocRequest deleteDocRequest) {
+		Mono<YuqueDeleteDocService.deleteDocResponse> deleteDocResponseMono = webClient.method(HttpMethod.DELETE)
+			.uri("/repos/{book_id}/docs/{id}", deleteDocRequest.bookId, deleteDocRequest.id)
+			.retrieve()
+			.bodyToMono(YuqueDeleteDocService.deleteDocResponse.class);
 
-        return deleteDocResponseMono.block();
-    }
+		return deleteDocResponseMono.block();
+	}
 
-    protected record deleteDocRequest(@JsonProperty("bookId") String bookId, @JsonProperty("id") String id) {}
+	protected record deleteDocRequest(@JsonProperty("bookId") String bookId, @JsonProperty("id") String id) {
+	}
 
-
-    protected record deleteDocResponse(@JsonProperty("id") String id, @JsonProperty("slug") String slug,
-                                       @JsonProperty("type") String type,
-                                       @JsonProperty("description") String description,
-                                       @JsonProperty("cover") String cover, @JsonProperty("user_id") String user_id,
-                                       @JsonProperty("book_id") String book_id,
-                                       @JsonProperty("last_editor_id") String last_editor_id,
-                                       @JsonProperty("format") String format,
-                                       @JsonProperty("body_draft") String body_draft,
-                                       @JsonProperty("body_sheet") String body_sheet,
-                                       @JsonProperty("body_table") String body_table,
-                                       @JsonProperty("body_html") String body_html,
-                                       @JsonProperty("public") int isPublic, @JsonProperty("status") String status,
-                                       @JsonProperty("likes_count") int likes_count,
-                                       @JsonProperty("read_count") int read_count,
-                                       @JsonProperty("comments_count") int comments_count,
-                                       @JsonProperty("word_count") int word_count,
-                                       @JsonProperty("created_at") String createdAt,
-                                       @JsonProperty("updated_at") String updatedAt) {}
+	protected record deleteDocResponse(@JsonProperty("id") String id, @JsonProperty("slug") String slug,
+			@JsonProperty("type") String type, @JsonProperty("description") String description,
+			@JsonProperty("cover") String cover, @JsonProperty("user_id") String user_id,
+			@JsonProperty("book_id") String book_id, @JsonProperty("last_editor_id") String last_editor_id,
+			@JsonProperty("format") String format, @JsonProperty("body_draft") String body_draft,
+			@JsonProperty("body_sheet") String body_sheet, @JsonProperty("body_table") String body_table,
+			@JsonProperty("body_html") String body_html, @JsonProperty("public") int isPublic,
+			@JsonProperty("status") String status, @JsonProperty("likes_count") int likes_count,
+			@JsonProperty("read_count") int read_count, @JsonProperty("comments_count") int comments_count,
+			@JsonProperty("word_count") int word_count, @JsonProperty("created_at") String createdAt,
+			@JsonProperty("updated_at") String updatedAt) {
+	}
 
 }
