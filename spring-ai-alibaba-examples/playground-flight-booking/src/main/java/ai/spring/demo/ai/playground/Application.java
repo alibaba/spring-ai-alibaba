a8ce730c17c690baf9ab43ae3c0e840a2ebc1fae
helltab <helltab@163.com>
2024-12-25 14:46:55 +0800

    fix(doc) Eliminate redundant commits

--- spring-ai-alibaba-examples/playground-flight-booking/src/main/java/ai/spring/demo/ai/playground/Application.java
+++ spring-ai-alibaba-examples/playground-flight-booking/src/main/java/ai/spring/demo/ai/playground/Application.java
@@ -1,11 +1,9 @@
 package ai.spring.demo.ai.playground;
 
-import com.alibaba.cloud.ai.dashscope.chat.DashScopeChatModel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.memory.InMemoryChatMemory;
-import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.embedding.EmbeddingModel;
 import org.springframework.ai.reader.TextReader;
 import org.springframework.ai.transformer.splitter.TokenTextSplitter;
@@ -14,11 +12,9 @@ import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.Resource;
-import org.springframework.web.client.RestClient;
 
 
 @SpringBootApplication
@@ -30,12 +26,11 @@ public class Application  {
 		new SpringApplicationBuilder(Application.class).run(args);
 	}
 
-
 	// In the real world, ingesting documents would often happen separately, on a CI
 	// server or similar.
 	@Bean
 	CommandLineRunner ingestTermOfServiceToVectorStore(EmbeddingModel embeddingModel, VectorStore vectorStore,
-			@Value("classpath:rag/terms-of-service.txt") Resource termsOfServiceDocs) {
+													   @Value("classpath:rag/terms-of-service.txt") Resource termsOfServiceDocs) {
 
 		return args -> {
 			// Ingest the document into the vector store
@@ -57,9 +52,4 @@ public class Application  {
 		return new InMemoryChatMemory();
 	}
 
-	@Bean
-	@ConditionalOnMissingBean
-	public RestClient.Builder restClientBuilder() {
-		return RestClient.builder();
-	}
 }
