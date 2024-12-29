2460f8514ec58176577fce2a6b64ca379dff827a
gronk <jianyi_gronk@163.com>
2024-12-29 20:39:09 +0800

    feat: 完善枚举

--- spring-ai-alibaba-graph/spring-ai-alibaba-graph-studio/graph-ui/src/pages/Graph/Design/types.ts
+++ spring-ai-alibaba-graph/spring-ai-alibaba-graph-studio/graph-ui/src/pages/Graph/Design/types.ts
@@ -41,15 +41,15 @@ export type MenuItem = Required<MenuProps>['items'][number];
 export type IGraphMenuItems = Array<MenuItem & { onClick: (e: TODO) => void }>;
 
 export enum ZoomType {
-  zoomIn = 'zoomIn',
-  zoomOut = 'zoomOut',
-  zoomToFit = 'zoomToFit',
-  zoomTo25 = 'zoomTo25',
-  zoomTo50 = 'zoomTo50',
-  zoomTo75 = 'zoomTo75',
-  zoomTo100 = 'zoomTo100',
-  zoomTo150 = 'zoomTo150',
-  zoomTo200 = 'zoomTo200',
+  ZOOM_TO_25 = 'zoomTo25',
+  ZOOM_TO_50 = 'zoomTo50',
+  ZOOM_TO_75 = 'zoomTo75',
+  ZOOM_TO_100 = 'zoomTo100',
+  ZOOM_TO_150 = 'zoomTo150',
+  ZOOM_TO_200 = 'zoomTo200',
 }
 
-export type OperationMode = 'hand' | 'pointer';
+export enum OperationMode {
+  HAND = 'hand',
+  POINT = 'pointer',
+}
