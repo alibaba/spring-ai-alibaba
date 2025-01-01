9eb848a42a9b04363dc201f307e9801f0546195d
wblu214 <wblu.eric@gmail.com>
2025-01-01 21:19:46 +0800

    feishu document reader end fix

--- community/document-readers/feishu-document-reader/src/main/java/com/alibaba/cloud/ai/reader/feishu/FeiShuResource.java
+++ community/document-readers/feishu-document-reader/src/main/java/com/alibaba/cloud/ai/reader/feishu/FeiShuResource.java
@@ -12,99 +12,113 @@ import java.net.URI;
 import java.net.URL;
 
 public class FeiShuResource implements Resource {
-    public static final String SOURCE = "source";
-    public static final String FEISHU_PROPERTIES_PREFIX = "spring.ai.alibaba.plugin.feishu";
-    private final String appId;
-    private final String appSecret;
 
-    public String getAppId() {
-        return appId;
-    }
+	public static final String SOURCE = "source";
 
-    public String getAppSecret() {
-        return appSecret;
-    }
-    public FeiShuResource(String appId, String appSecret) {
-        this.appId = appId;
-        this.appSecret = appSecret;
-    }
-    public static Builder builder() {
-        return new Builder();
-    }
+	public static final String FEISHU_PROPERTIES_PREFIX = "spring.ai.alibaba.plugin.feishu";
 
-    public static class Builder {
-        private  String appId;
-        private  String appSecret;
-        public Builder appId(String appId) {
-            this.appId = appId;
-            return this;
-        }
-        public Builder appSecret(String appSecret) {
-            this.appSecret = appSecret;
-            return this;
-        }
+	private final String appId;
 
-        public FeiShuResource build() {
-            Assert.notNull(appId, "FeiShu AppId must not be empty");
-            Assert.notNull(appSecret, "FeiShu AppSecret must not be empty");
-            return new FeiShuResource(appId, appSecret);
-        }
-    }
-    public Client buildDefaultFeiShuClient() {
-        Assert.notNull(this.appId, "FeiShu AppId must not be empty");
-        Assert.notNull(this.appSecret, "FeiShu AppSecret must not be empty");
-        return Client.newBuilder(this.appId,this.appSecret)
-                .openBaseUrl(BaseUrlEnum.FeiShu)
-                .logReqAtDebug(true)
-                .build();
-    }
-    @Override
-    public boolean exists() {
-        return false;
-    }
+	private final String appSecret;
 
-    @Override
-    public URL getURL() throws IOException {
-        return null;
-    }
+	public String getAppId() {
+		return appId;
+	}
 
-    @Override
-    public URI getURI() throws IOException {
-        return null;
-    }
+	public String getAppSecret() {
+		return appSecret;
+	}
 
-    @Override
-    public File getFile() throws IOException {
-        return null;
-    }
+	public FeiShuResource(String appId, String appSecret) {
+		this.appId = appId;
+		this.appSecret = appSecret;
+	}
 
-    @Override
-    public long contentLength() throws IOException {
-        return 0;
-    }
+	public static Builder builder() {
+		return new Builder();
+	}
 
-    @Override
-    public long lastModified() throws IOException {
-        return 0;
-    }
+	public static class Builder {
 
-    @Override
-    public Resource createRelative(String relativePath) throws IOException {
-        return null;
-    }
+		private String appId;
 
-    @Override
-    public String getFilename() {
-        return null;
-    }
+		private String appSecret;
 
-    @Override
-    public String getDescription() {
-        return null;
-    }
+		public Builder appId(String appId) {
+			this.appId = appId;
+			return this;
+		}
 
-    @Override
-    public InputStream getInputStream() throws IOException {
-        return null;
-    }
+		public Builder appSecret(String appSecret) {
+			this.appSecret = appSecret;
+			return this;
+		}
+
+		public FeiShuResource build() {
+			Assert.notNull(appId, "FeiShu AppId must not be empty");
+			Assert.notNull(appSecret, "FeiShu AppSecret must not be empty");
+			return new FeiShuResource(appId, appSecret);
+		}
+
+	}
+
+	public Client buildDefaultFeiShuClient() {
+		Assert.notNull(this.appId, "FeiShu AppId must not be empty");
+		Assert.notNull(this.appSecret, "FeiShu AppSecret must not be empty");
+		return Client.newBuilder(this.appId, this.appSecret)
+			.openBaseUrl(BaseUrlEnum.FeiShu)
+			.logReqAtDebug(true)
+			.build();
+	}
+
+	@Override
+	public boolean exists() {
+		return false;
+	}
+
+	@Override
+	public URL getURL() throws IOException {
+		return null;
+	}
+
+	@Override
+	public URI getURI() throws IOException {
+		return null;
+	}
+
+	@Override
+	public File getFile() throws IOException {
+		return null;
+	}
+
+	@Override
+	public long contentLength() throws IOException {
+		return 0;
+	}
+
+	@Override
+	public long lastModified() throws IOException {
+		return 0;
+	}
+
+	@Override
+	public Resource createRelative(String relativePath) throws IOException {
+		return null;
+	}
+
+	@Override
+	public String getFilename() {
+		return null;
+	}
+
+	@Override
+	public String getDescription() {
+		return null;
+	}
+
+	@Override
+	public InputStream getInputStream() throws IOException {
+		return null;
+	}
+
 }
