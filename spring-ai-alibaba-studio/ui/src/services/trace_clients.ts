e3ff96941e974204cf6e2aa179dc269797fef7d1
gronk <jianyi_gronk@163.com>
2025-01-02 00:03:55 +0800

    feat: 添加 trace 接口

--- spring-ai-alibaba-studio/ui/src/services/trace_clients.ts
+++ spring-ai-alibaba-studio/ui/src/services/trace_clients.ts
@@ -1,8 +1,13 @@
 import { request } from 'ice';
 
 export default {
-    async getTraceDetailClient() {
-        return await request('/studio/api/observation/getAITraceInfo');
-    },
+  async getTraceDetailClient() {
+    return await request('/studio/api/observation/getAITraceInfo');
+  },
+  async getTraceDetailClientById(traceId: string) {
+    return await request({
+      url: `/studio/api/observation/detail?traceId=${traceId}`,
+      method: 'get',
+    });
+  },
 };
-
