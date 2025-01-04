7cf2493b64629af1e2d6d8c5fc1cef1df43867cb
conghuhu <15841721425@163.com>
2025-01-04 22:51:12 +0800

    chat内存后端识别

--- spring-ai-alibaba-examples/cli-debug-example/src/main/java/com/alibaba/cloud/ai/example/cli/clidebugexample/CliDebugExampleApplication.java
+++ spring-ai-alibaba-examples/cli-debug-example/src/main/java/com/alibaba/cloud/ai/example/cli/clidebugexample/CliDebugExampleApplication.java
@@ -1,6 +1,9 @@
 package com.alibaba.cloud.ai.example.cli.clidebugexample;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.InMemoryChatMemory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -12,7 +15,10 @@ public class CliDebugExampleApplication {
 
     @Bean
     ChatClient chatClient(ChatClient.Builder builder) {
-        return builder.build();
+        ChatMemory chatMemory = new InMemoryChatMemory();
+        return builder
+            .defaultAdvisors(new MessageChatMemoryAdvisor(chatMemory))
+            .build();
     }
 
     public static void main(String[] args) {
