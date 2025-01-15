2315c4255def0b0d4453017b50e3a07897b96ece
PolarishT <zhangzhenting@corp.netease.com>
2025-01-15 19:19:05 +0800

    add Test License header
    Signed-off-by: PolarishT <zhangzhenting@corp.netease.com>

--- spring-ai-alibaba-core/src/main/java/com/alibaba/cloud/ai/advisor/RetrievalRerankAdvisor.java
+++ spring-ai-alibaba-core/src/main/java/com/alibaba/cloud/ai/advisor/RetrievalRerankAdvisor.java
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.alibaba.cloud.ai.advisor;
 
 import com.alibaba.cloud.ai.document.DocumentWithScore;
