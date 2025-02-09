5b992c9d0f946d53bcb763b8dfc0546e3677f8bc
yuluo-yx <yuluo08290126@gmail.com>
2025-02-09 20:59:40 +0800

    fix
    Signed-off-by: yuluo-yx <yuluo08290126@gmail.com>

--- spring-ai-alibaba-core/src/test/java/com/alibaba/cloud/ai/dashscope/chat/tool/DashScopeFunctionTestConfiguration.java
+++ spring-ai-alibaba-core/src/test/java/com/alibaba/cloud/ai/dashscope/chat/tool/DashScopeFunctionTestConfiguration.java
@@ -14,29 +14,13 @@
  * limitations under the License.
  */
 
-/*
- * Copyright 2024 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.alibaba.cloud.ai.dashscope.chat.tool;
+
+import java.util.function.Function;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Description;
-
-import java.util.function.Function;
 
 /**
  * Title Function Test configuration.<br>
