0a5c832dd1cfc6f50ab04ac7ee3ee3689009ca97
yuluo-yx <yuluo08290126@gmail.com>
2025-02-11 22:19:02 +0800

    feat: optimize spring-ai-alibaba-core
    Signed-off-by: yuluo-yx <yuluo08290126@gmail.com>

--- spring-ai-alibaba-core/src/main/java/com/alibaba/cloud/ai/dashscope/metadata/audio/DashScopeAudioSpeechResponseMetadata.java
+++ spring-ai-alibaba-core/src/main/java/com/alibaba/cloud/ai/dashscope/metadata/audio/DashScopeAudioSpeechResponseMetadata.java
@@ -15,9 +15,7 @@
  */
 package com.alibaba.cloud.ai.dashscope.metadata.audio;
 
-import com.alibaba.dashscope.audio.tts.SpeechSynthesisResult;
-import com.alibaba.dashscope.audio.tts.SpeechSynthesisUsage;
-import com.alibaba.dashscope.audio.tts.timestamp.Sentence;
+import com.alibaba.cloud.ai.dashscope.audio.synthesis.SpeechSynthesisResult;
 
 import org.springframework.ai.chat.metadata.EmptyRateLimit;
 import org.springframework.ai.chat.metadata.RateLimit;
@@ -33,90 +31,43 @@ import org.springframework.util.Assert;
 
 public class DashScopeAudioSpeechResponseMetadata extends MutableResponseMetadata {
 
-	/**
-	 * NULL objects.
-	 */
 	public static final DashScopeAudioSpeechResponseMetadata NULL = new DashScopeAudioSpeechResponseMetadata() {
 	};
 
 	protected static final String AI_METADATA_STRING = "{ @type: %1$s, requestsLimit: %2$s }";
 
-	private SpeechSynthesisUsage usage;
-
-	private String requestId;
-
-	private Sentence time;
-
 	@Nullable
 	private RateLimit rateLimit;
 
 	public DashScopeAudioSpeechResponseMetadata() {
-
 		this(null);
 	}
 
 	public DashScopeAudioSpeechResponseMetadata(@Nullable RateLimit rateLimit) {
-
 		this.rateLimit = rateLimit;
 	}
 
 	public static DashScopeAudioSpeechResponseMetadata from(SpeechSynthesisResult result) {
-
-		Assert.notNull(result, "DashScope AI speech must not be null");
+		Assert.notNull(result, "DashScope speech must not be null");
 		DashScopeAudioSpeechResponseMetadata speechResponseMetadata = new DashScopeAudioSpeechResponseMetadata();
-
 		return speechResponseMetadata;
 	}
 
 	public static DashScopeAudioSpeechResponseMetadata from(String result) {
-
-		Assert.notNull(result, "DashScope AI speech must not be null");
+		Assert.notNull(result, "DashScope speech must not be null");
 		DashScopeAudioSpeechResponseMetadata speechResponseMetadata = new DashScopeAudioSpeechResponseMetadata();
-
 		return speechResponseMetadata;
 	}
 
 	@Nullable
 	public RateLimit getRateLimit() {
-
 		RateLimit rateLimit = this.rateLimit;
 		return rateLimit != null ? rateLimit : new EmptyRateLimit();
 	}
 
 	public DashScopeAudioSpeechResponseMetadata withRateLimit(RateLimit rateLimit) {
-
 		this.rateLimit = rateLimit;
 		return this;
-	}
-
-	public DashScopeAudioSpeechResponseMetadata withUsage(SpeechSynthesisUsage usage) {
-
-		this.usage = usage;
-		return this;
-	}
-
-	public DashScopeAudioSpeechResponseMetadata withRequestId(String id) {
-
-		this.requestId = id;
-		return this;
-	}
-
-	public DashScopeAudioSpeechResponseMetadata withSentence(Sentence sentence) {
-
-		this.time = sentence;
-		return this;
-	}
-
-	public SpeechSynthesisUsage getUsage() {
-		return usage;
-	}
-
-	public String getRequestId() {
-		return requestId;
-	}
-
-	public Sentence getTime() {
-		return time;
 	}
 
 	@Override
