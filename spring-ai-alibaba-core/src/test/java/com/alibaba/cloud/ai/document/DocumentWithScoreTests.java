a4916f5d60e06ad70cdacbe53a07dc0c853ae9a4
Xia Dong <xiadong1234ac@163.com>
2025-02-26 17:51:38 +0800

    完成rerank的测试用例

--- spring-ai-alibaba-core/src/test/java/com/alibaba/cloud/ai/document/DocumentWithScoreTests.java
+++ spring-ai-alibaba-core/src/test/java/com/alibaba/cloud/ai/document/DocumentWithScoreTests.java
@@ -27,6 +27,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 /**
  * Test cases for DocumentWithScore
  *
+ * @author yuluo
+ * @author <a href="mailto:yuluo08290126@gmail.com">yuluo</a>
  * @author brianxiadong
  * @since 1.0.0-M5.1
  */
