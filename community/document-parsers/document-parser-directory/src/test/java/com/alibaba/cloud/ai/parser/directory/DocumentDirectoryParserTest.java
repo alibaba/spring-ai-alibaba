f840c649f51a99f86974ea7f5cd5a3b6e436303a
Xia Dong <xiadong1234ac@163.com>
2025-02-28 15:40:42 +0800

    修正一部分community中ut的问题

--- community/document-parsers/document-parser-directory/src/test/java/com/alibaba/cloud/ai/parser/directory/DocumentDirectoryParserTest.java
+++ community/document-parsers/document-parser-directory/src/test/java/com/alibaba/cloud/ai/parser/directory/DocumentDirectoryParserTest.java
@@ -29,63 +29,63 @@ import java.util.List;
 
 public class DocumentDirectoryParserTest {
 
-    // Load all non-hidden files in a directory.
-    @Test
-    public void testAllNoHidden() {
-        String path = "D:\\code\\paper_impl_and_git_code\\ai_framework\\spring-ai-alibaba\\community\\document-parsers\\document-parser-directory\\src\\test\\resources";
+	// Load all non-hidden files in a directory.
+	@Test
+	public void testAllNoHidden() {
+		String path = "D:\\code\\paper_impl_and_git_code\\ai_framework\\spring-ai-alibaba\\community\\document-parsers\\document-parser-directory\\src\\test\\resources";
 
-        DocumentDirectoryParser parser = new DocumentDirectoryParser.Builder(path)
-                .documentParser(new TextDocumentParser())
-                .build();
-        List<Document> documents = parser.parse();
-        for (Document document : documents) {
-            System.out.println(document.getText());
-        }
-    }
+		DocumentDirectoryParser parser = new DocumentDirectoryParser.Builder(path)
+			.documentParser(new TextDocumentParser())
+			.build();
+		List<Document> documents = parser.parse();
+		for (Document document : documents) {
+			System.out.println(document.getText());
+		}
+	}
 
-    // Load all text files in a directory without recursion.
-    @Test
-    public void testAllTextNoHidden() {
-        String path = "D:\\code\\paper_impl_and_git_code\\ai_framework\\spring-ai-alibaba\\community\\document-parsers\\document-parser-directory\\src\\test\\resources";
+	// Load all text files in a directory without recursion.
+	@Test
+	public void testAllTextNoHidden() {
+		String path = "D:\\code\\paper_impl_and_git_code\\ai_framework\\spring-ai-alibaba\\community\\document-parsers\\document-parser-directory\\src\\test\\resources";
 
-        DocumentDirectoryParser parser = new DocumentDirectoryParser.Builder(path)
-                .documentParser(new TextDocumentParser())
-                .glob("*.txt")
-                .build();
-        List<Document> documents = parser.parse();
-        for (Document document : documents) {
-            System.out.println(document.getText());
-        }
-    }
+		DocumentDirectoryParser parser = new DocumentDirectoryParser.Builder(path)
+			.documentParser(new TextDocumentParser())
+			.glob("*.txt")
+			.build();
+		List<Document> documents = parser.parse();
+		for (Document document : documents) {
+			System.out.println(document.getText());
+		}
+	}
 
-    // Recursively load all text files in a directory.
-    @Test
-    public void testAllTextRecursive() {
-        String path = "D:\\code\\paper_impl_and_git_code\\ai_framework\\spring-ai-alibaba\\community\\document-parsers\\document-parser-directory\\src\\test\\resources";
-        DocumentDirectoryParser parser = new DocumentDirectoryParser.Builder(path)
-                .documentParser(new TextDocumentParser())
-                .glob("*.txt")
-                .recursive(true)
-                .build();
-        List<Document> documents = parser.parse();
-        for (Document document : documents) {
-            System.out.println(document.getText());
-        }
-    }
+	// Recursively load all text files in a directory.
+	@Test
+	public void testAllTextRecursive() {
+		String path = "D:\\code\\paper_impl_and_git_code\\ai_framework\\spring-ai-alibaba\\community\\document-parsers\\document-parser-directory\\src\\test\\resources";
+		DocumentDirectoryParser parser = new DocumentDirectoryParser.Builder(path)
+			.documentParser(new TextDocumentParser())
+			.glob("*.txt")
+			.recursive(true)
+			.build();
+		List<Document> documents = parser.parse();
+		for (Document document : documents) {
+			System.out.println(document.getText());
+		}
+	}
 
-    // Load all files in a directory, except for py files.
-    @Test
-    public void testExceptNoHidden() {
-        String path = "D:\\code\\paper_impl_and_git_code\\ai_framework\\spring-ai-alibaba\\community\\document-parsers\\document-parser-directory\\src\\test\\resources";
+	// Load all files in a directory, except for py files.
+	@Test
+	public void testExceptNoHidden() {
+		String path = "D:\\code\\paper_impl_and_git_code\\ai_framework\\spring-ai-alibaba\\community\\document-parsers\\document-parser-directory\\src\\test\\resources";
 
-        DocumentDirectoryParser parser = new DocumentDirectoryParser.Builder(path)
-                .documentParser(new TextDocumentParser())
-                .exclude("*.py")
-                .build();
-        List<Document> documents = parser.parse();
-        for (Document document : documents) {
-            System.out.println(document.getText());
-        }
-    }
+		DocumentDirectoryParser parser = new DocumentDirectoryParser.Builder(path)
+			.documentParser(new TextDocumentParser())
+			.exclude("*.py")
+			.build();
+		List<Document> documents = parser.parse();
+		for (Document document : documents) {
+			System.out.println(document.getText());
+		}
+	}
 
 }
