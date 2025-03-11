eef01d8946fd36b7b3b2f8e7074ab86216e95705
yuhuangbin <yuhuangbin@bwton.com>
2025-03-11 11:56:35 +0800

    dependency fix

--- community/tool-calls/spring-ai-alibaba-starter-tool-calling-microsofttranslate/src/main/java/com/alibaba/cloud/ai/toolcalling/microsofttranslate/MicroSoftTranslateAutoConfiguration.java
+++ community/tool-calls/spring-ai-alibaba-starter-tool-calling-microsofttranslate/src/main/java/com/alibaba/cloud/ai/toolcalling/microsofttranslate/MicroSoftTranslateAutoConfiguration.java
@@ -17,7 +17,6 @@ package com.alibaba.cloud.ai.toolcalling.microsofttranslate;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -29,8 +28,6 @@ import org.springframework.context.annotation.Description;
 @Configuration
 @ConditionalOnClass(MicroSoftTranslateService.class)
 @EnableConfigurationProperties(MicroSoftTranslateProperties.class)
-//@ConditionalOnProperty(prefix = "spring.ai.alibaba.toolcalling.microsofttranslate", name = "enabled",
-//		havingValue = "true")
 public class MicroSoftTranslateAutoConfiguration {
 
 	@Bean
