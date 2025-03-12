60f97bf3cabd9ac959f89657606f85cbd136f025
Disaster <58838568+disaster1-tesk@users.noreply.github.com>
2025-03-12 11:38:26 +0800

    :busts_in_silhouette: Example
    child to child example add

--- spring-ai-alibaba-graph/spring-ai-alibaba-graph-core/src/test/java/com/alibaba/cloud/ai/graph/CommandGraphTest.java
+++ spring-ai-alibaba-graph/spring-ai-alibaba-graph-core/src/test/java/com/alibaba/cloud/ai/graph/CommandGraphTest.java
@@ -72,4 +72,51 @@ public class CommandGraphTest {
         var app = workflowParent.compile();
         _execute(app, Map.of());
     }
+
+    @Test
+    public void testCommandSubgraph03() throws Exception {
+
+        var workflowChild = new StateGraph().addNode("B1", _makeNode("B1"))
+                .addNode("B2", _makeNode("B2"))
+                .addCommandNode("command", (t, config) -> {
+                    Command command = new Command();
+                    // If graph is of the parent type, nodeId is not required
+                    command.setGraph(Command.GraphType.PARENT);
+                    command.setEdge("A");
+                    // Additional parameters can be added to the command object
+                    command.put("messages","command");
+                    return command;
+                })
+                .addEdge(START, "command")
+                .addEdge("B1",END);
+
+        var workflowChild2 = new StateGraph().addNode("B1", _makeNode("B1"))
+                .addNode("B2", _makeNode("B2"))
+                .addCommandNode("command", (t, config) -> {
+                    Command command = new Command();
+                    // If graph is of the parent type, nodeId is not required
+                    command.setGraph(Command.GraphType.CHILD);
+                    command.setNodeId("B");
+                    command.setEdge("B1");
+                    // Additional parameters can be added to the command object
+                    command.put("messages","command");
+                    return command;
+                })
+                .addEdge(START, "command")
+                .addEdge("B1",END);
+
+        var workflowParent = new StateGraph(getOverAllState()).addNode("A", _makeNode("A"))
+                .addSubgraph("B", workflowChild)
+                .addNode("C", _makeNode("C"))
+                .addSubgraph("D", workflowChild2)
+                .addEdge(START, "C")
+                .addEdge("C", "B")
+                .addEdge("B","A")
+                .addEdge("A","D")
+                .addEdge("D", END);
+
+
+        var app = workflowParent.compile();
+        _execute(app, Map.of());
+    }
 }
