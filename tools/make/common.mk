70a13b56311b9f09f67d2a7d4c250abb199d1246
yuluo-yx <yuluo08290126@gmail.com>
2025-03-14 21:33:57 +0800

    fix
    Signed-off-by: yuluo-yx <yuluo08290126@gmail.com>

--- tools/make/common.mk
+++ tools/make/common.mk
@@ -28,3 +28,11 @@ endef
 define errorLog
 echo -e "\033[0;31m==================>$1\033[0m"
 endef
+
+##@ Common
+
+.PHONY: help
+help:
+	@echo -e "\033[1;3;34mAn Application Framework for Java Developers.\033[0m\n"
+	@echo -e "Usage:\n  make \033[36m<Target>\033[0m \033[36m<Option>\033[0m\n\nTargets:"
+	@awk 'BEGIN {FS = ":.*##"; printf ""} /^[a-zA-Z_0-9-]+:.*?##/ { printf "  \033[36m%-15s\033[0m %s\n", $$1, $$2 } /^##@/ { printf "\n\033[1m%s\033[0m\n", substr($$0, 5) } ' $(MAKEFILE_LIST)
