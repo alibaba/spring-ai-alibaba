379516b80df5dd18b3e6cb8958558d1b12831182
yingzi <GTyingzi@gmail.com>
2025-03-23 18:02:00 +0800

    update

--- community/memories/spring-ai-alibaba-mysql-memory/src/main/java/com/alibaba/cloud/ai/memory/mysql/serializer/MessageDeserializer.java
+++ community/memories/spring-ai-alibaba-mysql-memory/src/main/java/com/alibaba/cloud/ai/memory/mysql/serializer/MessageDeserializer.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2024-2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.alibaba.cloud.ai.memory.mysql.serializer;
 
 import com.fasterxml.jackson.core.JsonParser;
@@ -17,10 +32,6 @@ import java.io.IOException;
 import java.util.Collection;
 import java.util.Map;
 
-/**
- * @author yingzi
- * @date 2025/3/23:15:12
- */
 public class MessageDeserializer extends JsonDeserializer<Message> {
 
 	private static final Logger logger = LoggerFactory.getLogger(MessageDeserializer.class);
