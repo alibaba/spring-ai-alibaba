c7cac4b14d3ba0388a9b15ac300fb236c157e561
WhisperXD <answeropensource@alibabacloud.com>
2025-03-24 11:28:30 +0800

    feat: 优化工具参数和描述格式，提升代码可读性

--- community/openmanus/src/main/java/com/alibaba/cloud/ai/example/manus/tool/PythonExecute.java
+++ community/openmanus/src/main/java/com/alibaba/cloud/ai/example/manus/tool/PythonExecute.java
@@ -41,16 +41,34 @@ public class PythonExecute implements BiFunction<String, ToolContext, ToolExecut
 
 	private Boolean arm64 = true;
 
-	public static final String LLMMATH_PYTHON_CODE = "import sys; import math; import numpy as np; import numexpr as ne; input = '%s'; res = ne.evaluate(input); print(res)";
+	public static final String LLMMATH_PYTHON_CODE = """
+        import sys
+        import math
+        import numpy as np
+        import numexpr as ne
+        input = '%s'
+        res = ne.evaluate(input)
+        print(res)
+        """;
 
-	private static String PARAMETERS = "{\n" + "\t\"type\": \"object\",\n" + "\t\"properties\": {\n"
-			+ "\t\t\"code\": {\n" + "\t\t\t\"type\": \"string\",\n"
-			+ "\t\t\t\"description\": \"The Python code to execute.\"\n" + "\t\t}\n" + "\t},\n"
-			+ "\t\"required\": [\"code\"]\n" + "}";
+	private static String PARAMETERS = """
+        {
+            "type": "object",
+            "properties": {
+                "code": {
+                    "type": "string",
+                    "description": "The Python code to execute."
+                }
+            },
+            "required": ["code"]
+        }
+        """;
 
 	private static final String name = "python_execute";
 
-	private static final String description = "Executes Python code string. Note: Only print outputs are visible, function return values are not captured. Use print statements to see results.";
+	private static final String description = """
+        Executes Python code string. Note: Only print outputs are visible, function return values are not captured. Use print statements to see results.
+        """;
 
 	public static OpenAiApi.FunctionTool getToolDefinition() {
 		OpenAiApi.FunctionTool.Function function = new OpenAiApi.FunctionTool.Function(description, name, PARAMETERS);
