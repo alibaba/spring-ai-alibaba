e488a40166e8273b3b916dfc94a68d4448a76522
PolarishT <zhenting.zhang@student.unsw.edu.au>
2025-03-25 21:16:55 +0800

    code format
    Signed-off-by: PolarishT <zhenting.zhang@student.unsw.edu.au>

--- community/openmanus/README.md
+++ community/openmanus/README.md
@@ -34,5 +34,5 @@ mvn spring-boot:run
 ```
 
 ## Architecture
-![aaa](https://github.com/user-attachments/assets/4ad14a72-667b-456e-85c1-b05eef8fd414)
 
+![aaa](https://github.com/user-attachments/assets/4ad14a72-667b-456e-85c1-b05eef8fd414)
--- tools/make/linter.mk
+++ tools/make/linter.mk
@@ -1,3 +1,17 @@
+# Copyright 2024-2026 the original author or authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # Licensed to the Apache Software Foundation (ASF) under one
 # or more contributor license agreements.  See the NOTICE file
 # distributed with this work for additional information
@@ -17,18 +31,6 @@
 
 #
 # Copyright 2024-2026 the original author or authors.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     https://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 #
 
 ##@ Linter
@@ -55,14 +57,14 @@ yaml-lint: ## Check the yaml lint
 .PHONY: licenses-fix
 licenses-fix: ## Fix the licenses
 	@$(LOG_TARGET)
-	licenses-eye --version
-	licenses-eye -c ./tools/linter/license/.licenserc.yaml header fix
+	license-eye --version
+	license-eye -c ./tools/linter/license/.licenserc.yaml header fix
 
 .PHONY: licenses-check
 licenses-check: ## Check the licenses
 	@$(LOG_TARGET)
-	licenses-eye --version
-	licenses-eye -c ./tools/linter/license/.licenserc.yaml header check
+	license-eye --version
+	license-eye -c ./tools/linter/license/.licenserc.yaml header check
 
 .PHONY: markdown-lint
 markdown-lint: ## Check the markdown files.
