6e91397d504065d69d9be28f8ab5c9ac693d4056
future0923 <future94@qq.com>
2025-03-25 14:24:42 +0800

    mvn spring-javaformat:apply

--- community/memories/spring-ai-alibaba-jdbc-memory/src/test/java/com/alibaba/cloud/ai/memory/jdbc/MysqlChatMemoryTest.java
+++ community/memories/spring-ai-alibaba-jdbc-memory/src/test/java/com/alibaba/cloud/ai/memory/jdbc/MysqlChatMemoryTest.java
@@ -13,23 +13,25 @@ import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
  */
 class MysqlChatMemoryTest {
 
-    @Test
-    public void mysql() {
+	@Test
+	public void mysql() {
 
-        MysqlChatMemory chatMemory = new MysqlChatMemory("root", "123456", "jdbc:mysql://127.0.0.1:3306/spring_ai_alibaba_chat_memory");
-        ChatClient chatClient = ChatClient.create(new DashScopeChatModel(new DashScopeApi("")));
-        String content1 = chatClient.prompt()
-                .advisors(new MessageChatMemoryAdvisor(chatMemory))
-                .user("我是张三😄")
-                .call()
-                .content();
-        System.out.println(content1);
-        String content2 = chatClient.prompt()
-                .advisors(new MessageChatMemoryAdvisor(chatMemory))
-                .user("我是谁")
-                .call()
-                .content();
-        System.out.println(content2);
-        Assertions.assertTrue(content2.contains("张三"));
-    }
+		MysqlChatMemory chatMemory = new MysqlChatMemory("root", "123456",
+				"jdbc:mysql://127.0.0.1:3306/spring_ai_alibaba_chat_memory");
+		ChatClient chatClient = ChatClient.create(new DashScopeChatModel(new DashScopeApi("")));
+		String content1 = chatClient.prompt()
+			.advisors(new MessageChatMemoryAdvisor(chatMemory))
+			.user("我是张三😄")
+			.call()
+			.content();
+		System.out.println(content1);
+		String content2 = chatClient.prompt()
+			.advisors(new MessageChatMemoryAdvisor(chatMemory))
+			.user("我是谁")
+			.call()
+			.content();
+		System.out.println(content2);
+		Assertions.assertTrue(content2.contains("张三"));
+	}
+
 }
