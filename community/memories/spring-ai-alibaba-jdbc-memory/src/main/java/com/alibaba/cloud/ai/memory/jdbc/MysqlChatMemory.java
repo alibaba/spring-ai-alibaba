2ea7b634c2bc034ba9cb6ffcb4d3860db88c9248
future0923 <future94@qq.com>
2025-03-25 14:08:24 +0800

    update mysql to jdbc chat memory

---
+++ community/memories/spring-ai-alibaba-jdbc-memory/src/main/java/com/alibaba/cloud/ai/memory/jdbc/MysqlChatMemory.java
@@ -0,0 +1,43 @@
+package com.alibaba.cloud.ai.memory.jdbc;
+
+import java.sql.Connection;
+
+/**
+ * @author future0923
+ */
+public class MysqlChatMemory extends JdbcChatMemory {
+
+    private static final String JDBC_TYPE = "mysql";
+
+
+    public MysqlChatMemory(String username, String password, String jdbcUrl) {
+        super(username, password, jdbcUrl);
+    }
+
+    public MysqlChatMemory(String username, String password, String jdbcUrl, String tableName) {
+        super(username, password, jdbcUrl, tableName);
+    }
+
+    public MysqlChatMemory(Connection connection) {
+        super(connection);
+    }
+
+    public MysqlChatMemory(Connection connection, String tableName) {
+        super(connection, tableName);
+    }
+
+    @Override
+    protected String jdbcType() {
+        return JDBC_TYPE;
+    }
+
+    @Override
+    protected String hasTableSql(String tableName) {
+        return String.format("SHOW TABLES LIKE '%s'", tableName);
+    }
+
+    @Override
+    protected String createTableSql(String tableName) {
+        return String.format("CREATE TABLE %s( id BIGINT AUTO_INCREMENT PRIMARY KEY,conversation_id  VARCHAR(256)  NULL,messages TEXT NULL,UNIQUE (conversation_id)) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;", tableName);
+    }
+}
---
+++ community/memories/spring-ai-alibaba-jdbc-memory/src/test/java/com/alibaba/cloud/ai/memory/jdbc/OracleChatMemoryTest.java
@@ -0,0 +1,35 @@
+package com.alibaba.cloud.ai.memory.jdbc;
+
+import com.alibaba.cloud.ai.dashscope.api.DashScopeApi;
+import com.alibaba.cloud.ai.dashscope.chat.DashScopeChatModel;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+
+/**
+ * @author future0923
+ *
+ */
+class OracleChatMemoryTest {
+
+    @Test
+    public void oracle() {
+        ChatMemory chatMemory = new OracleChatMemory("system", "123456", "jdbc:oracle:thin:@localhost:1521/XEPDB1");
+        ChatClient chatClient = ChatClient.create(new DashScopeChatModel(new DashScopeApi("")));
+        String content1 = chatClient.prompt()
+                .advisors(new MessageChatMemoryAdvisor(chatMemory))
+                .user("我是张三😄")
+                .call()
+                .content();
+        System.out.println(content1);
+        String content2 = chatClient.prompt()
+                .advisors(new MessageChatMemoryAdvisor(chatMemory))
+                .user("我是谁")
+                .call()
+                .content();
+        System.out.println(content2);
+        Assertions.assertTrue(content2.contains("张三"));
+    }
+}
