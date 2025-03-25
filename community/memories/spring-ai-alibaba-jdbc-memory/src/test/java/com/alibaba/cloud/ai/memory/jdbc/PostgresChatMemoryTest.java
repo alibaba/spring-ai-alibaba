10c3059b756847630d655da0d3437eb6131c6283
future0923 <future94@qq.com>
2025-03-25 14:34:58 +0800

    add license

--- community/memories/spring-ai-alibaba-jdbc-memory/src/test/java/com/alibaba/cloud/ai/memory/jdbc/PostgresChatMemoryTest.java
+++ community/memories/spring-ai-alibaba-jdbc-memory/src/test/java/com/alibaba/cloud/ai/memory/jdbc/PostgresChatMemoryTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2024-2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.alibaba.cloud.ai.memory.jdbc;
 
 import com.alibaba.cloud.ai.dashscope.api.DashScopeApi;
@@ -15,7 +30,7 @@ import org.springframework.ai.chat.memory.ChatMemory;
 class PostgresChatMemoryTest {
 
 	@Test
-	public void postgresSQL() {
+	public void postgresql() {
 		ChatMemory chatMemory = new PostgresChatMemory("root", "123456",
 				"jdbc:postgresql://127.0.0.1:5432/spring_ai_alibaba_chat_memory");
 		ChatClient chatClient = ChatClient.create(new DashScopeChatModel(new DashScopeApi("")));
