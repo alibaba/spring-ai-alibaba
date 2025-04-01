934b5dda52bbe118b4a8e9a4077a0b975cbc079d
Zhenting Zhang <zhenting.zhang@student.unsw.edu.au>
2025-04-01 21:13:26 +0800

    [WIP TC]: Fix toolcalling (#557)
    * fix: fix annotation
    
    Signed-off-by: PolarishT <zhenting.zhang@student.unsw.edu.au>
    
    * remove useless conf
    
    Signed-off-by: PolarishT <zhenting.zhang@student.unsw.edu.au>
    
    * code format
    
    Signed-off-by: PolarishT <zhenting.zhang@student.unsw.edu.au>

--- community/tool-calls/spring-ai-alibaba-starter-tool-calling-googletranslate/src/main/java/com/alibaba/cloud/ai/toolcalling/googletranslate/GoogleTranslateAutoConfiguration.java
+++ community/tool-calls/spring-ai-alibaba-starter-tool-calling-googletranslate/src/main/java/com/alibaba/cloud/ai/toolcalling/googletranslate/GoogleTranslateAutoConfiguration.java
@@ -20,11 +20,13 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Description;
 
 /**
  * @author erasernoob
  */
+@Configuration
 @ConditionalOnClass({ GoogleTranslateService.class })
 @EnableConfigurationProperties(GoogleTranslateProperties.class)
 @ConditionalOnProperty(prefix = "spring.ai.alibaba.toolcalling.googletranslate", name = "enabled", havingValue = "true")
