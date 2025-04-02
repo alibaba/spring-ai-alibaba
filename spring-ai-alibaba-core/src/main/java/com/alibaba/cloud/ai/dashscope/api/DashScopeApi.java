0ac7e6207122e2fd0bf0edd8581db62eda987d39
miaohai <timothy_huo@aliyun.com>
2025-04-02 11:07:00 +0800

    DashScopeApi 增加对 SearchFilters 参数的支持 ..

--- spring-ai-alibaba-core/src/main/java/com/alibaba/cloud/ai/dashscope/api/DashScopeApi.java
+++ spring-ai-alibaba-core/src/main/java/com/alibaba/cloud/ai/dashscope/api/DashScopeApi.java
@@ -567,7 +567,8 @@ public class DashScopeApi {
 					@JsonProperty("enable_reranking") boolean enableRerank,
 					@JsonProperty("rerank") List<CommonModelComponent> rerankComponents,
 					@JsonProperty("rerank_min_score") float rerankMinScore,
-					@JsonProperty("rerank_top_n") int rerankTopN) {
+					@JsonProperty("rerank_top_n") int rerankTopN,
+					@JsonProperty("search_filters") List<Map<String, Object>> searchFilters) {
 
 			}
 
@@ -621,7 +622,8 @@ public class DashScopeApi {
 			@JsonProperty("rewrite") List<DocumentRetrieveModelConfig> rewrite,
 			@JsonProperty("enable_reranking") boolean enableReranking,
 			@JsonProperty("rerank") List<DocumentRetrieveModelConfig> rerank,
-			@JsonProperty("rerank_min_score") float rerankMinScore, @JsonProperty("rerank_top_n") int rerankTopN) {
+			@JsonProperty("rerank_min_score") float rerankMinScore, @JsonProperty("rerank_top_n") int rerankTopN,
+			@JsonProperty("search_filters") List<Map<String, Object>> searchFilters) {
 		@JsonInclude(JsonInclude.Include.NON_NULL)
 		public record DocumentRetrieveModelConfig(@JsonProperty("model_name") String modelName,
 				@JsonProperty("class_name") String className) {
@@ -694,7 +696,8 @@ public class DashScopeApi {
 						retrieverOptions.isEnableReranking(),
 						Arrays.asList(new UpsertPipelineRequest.RetrieverConfiguredTransformations.CommonModelComponent(
 								retrieverOptions.getRerankModelName())),
-						retrieverOptions.getRerankMinScore(), retrieverOptions.getRerankTopN()));
+						retrieverOptions.getRerankMinScore(), retrieverOptions.getRerankTopN(),
+						retrieverOptions.getSearchFilters()));
 		List<String> documentIdList = documents.stream()
 			.map(Document::getId)
 			.filter(Objects::nonNull)
@@ -747,12 +750,13 @@ public class DashScopeApi {
 	public List<Document> retriever(String pipelineId, String query, DashScopeDocumentRetrieverOptions searchOption) {
 		DocumentRetrieveRequest request = new DocumentRetrieveRequest(query, searchOption.getDenseSimilarityTopK(),
 				searchOption.getDenseSimilarityTopK(), searchOption.isEnableRewrite(),
-				Arrays.asList(new DocumentRetrieveRequest.DocumentRetrieveModelConfig(
-						searchOption.getRewriteModelName(), "DashScopeTextRewrite")),
+				Arrays
+					.asList(new DocumentRetrieveRequest.DocumentRetrieveModelConfig(
+							searchOption.getRewriteModelName(), "DashScopeTextRewrite")),
 				searchOption.isEnableReranking(),
 				Arrays.asList(new DocumentRetrieveRequest.DocumentRetrieveModelConfig(searchOption.getRerankModelName(),
 						null)),
-				searchOption.getRerankMinScore(), searchOption.getRerankTopN());
+				searchOption.getRerankMinScore(), searchOption.getRerankTopN(), searchOption.getSearchFilters());
 		ResponseEntity<DocumentRetrieveResponse> deleDocumentResponse = this.restClient.post()
 			.uri("/api/v1/indices/pipeline/{pipeline_id}/retrieve", pipelineId)
 			.body(request)
