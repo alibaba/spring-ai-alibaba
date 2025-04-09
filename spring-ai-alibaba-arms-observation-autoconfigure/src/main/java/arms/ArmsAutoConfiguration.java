0dd868dd1fa27765c79476317d17508ce64f6ea8
cirilla-zmh <zmh1625@163.com>
2025-04-09 09:41:58 +0800

    add license headers and apply spring java format

--- spring-ai-alibaba-arms-observation-autoconfigure/src/main/java/arms/ArmsAutoConfiguration.java
+++ spring-ai-alibaba-arms-observation-autoconfigure/src/main/java/arms/ArmsAutoConfiguration.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2024-2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package arms;
 
 import com.alibaba.cloud.ai.tool.ObservableToolCallingManager;
@@ -18,18 +33,16 @@ import org.springframework.context.annotation.Bean;
 @EnableConfigurationProperties(ArmsCommonProperties.class)
 public class ArmsAutoConfiguration {
 
-  @Bean
-  @ConditionalOnProperty(
-      prefix = ArmsCommonProperties.CONFIG_PREFIX,
-      name = "enabled",
-      havingValue = "true")
-  ToolCallingManager toolCallingManager(ToolCallbackResolver toolCallbackResolver,
-      ToolExecutionExceptionProcessor toolExecutionExceptionProcessor,
-      ObjectProvider<ObservationRegistry> observationRegistry) {
-    return ObservableToolCallingManager.builder()
-        .observationRegistry(observationRegistry.getIfUnique(() -> ObservationRegistry.NOOP))
-        .toolCallbackResolver(toolCallbackResolver)
-        .toolExecutionExceptionProcessor(toolExecutionExceptionProcessor)
-        .build();
-  }
+	@Bean
+	@ConditionalOnProperty(prefix = ArmsCommonProperties.CONFIG_PREFIX, name = "enabled", havingValue = "true")
+	ToolCallingManager toolCallingManager(ToolCallbackResolver toolCallbackResolver,
+			ToolExecutionExceptionProcessor toolExecutionExceptionProcessor,
+			ObjectProvider<ObservationRegistry> observationRegistry) {
+		return ObservableToolCallingManager.builder()
+			.observationRegistry(observationRegistry.getIfUnique(() -> ObservationRegistry.NOOP))
+			.toolCallbackResolver(toolCallbackResolver)
+			.toolExecutionExceptionProcessor(toolExecutionExceptionProcessor)
+			.build();
+	}
+
 }
