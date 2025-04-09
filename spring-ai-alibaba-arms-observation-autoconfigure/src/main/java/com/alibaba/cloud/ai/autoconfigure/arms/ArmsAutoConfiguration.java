02c44d524100f2ef3b093ec97c84911e5a16e9d1
cirilla-zmh <zmh1625@163.com>
2025-04-09 11:45:38 +0800

    refactor the package name

--- spring-ai-alibaba-arms-observation-autoconfigure/src/main/java/com/alibaba/cloud/ai/autoconfigure/arms/ArmsAutoConfiguration.java
+++ spring-ai-alibaba-arms-observation-autoconfigure/src/main/java/com/alibaba/cloud/ai/autoconfigure/arms/ArmsAutoConfiguration.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package arms;
+package com.alibaba.cloud.ai.autoconfigure.arms;
 
 import com.alibaba.cloud.ai.tool.ObservableToolCallingManager;
 import io.micrometer.observation.ObservationRegistry;
