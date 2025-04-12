29e3f0e9d3b46ace7836c48ac7bf4af0b28a7629
VLSMB <2047857654@qq.com>
2025-04-13 02:01:38 +0800

    feat: add common module to community/tool-call

---
+++ community/tool-calls/spring-ai-alibaba-starter-tool-calling-commontoolcall/src/main/java/com/alibaba/cloud/ai/toolcalling/commontoolcall/RestClientService.java
@@ -0,0 +1,62 @@
+package com.alibaba.cloud.ai.toolcalling.commontoolcall;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.MediaType;
+import org.springframework.web.client.RestClient;
+
+import java.util.Map;
+import java.util.function.Consumer;
+
+public class RestClientService {
+
+	private final RestClient restClient;
+
+	private final JsonParseService jsonParseService;
+
+	public RestClientService(JsonParseService jsonParseService) {
+		this.jsonParseService = jsonParseService;
+		this.restClient = RestClient.builder().build();
+	}
+
+	public RestClientService(String baseUrl, Consumer<HttpHeaders> httpHeadersConsumer,
+			JsonParseService jsonParseService) {
+		this.jsonParseService = jsonParseService;
+		this.restClient = RestClient.builder().baseUrl(baseUrl).defaultHeaders(httpHeadersConsumer).build();
+	}
+
+	public String get(String uri, Map<String, Object> variables) {
+		return restClient.get()
+			.uri(uriBuilder -> uriBuilder.path(uri).build(variables))
+			.retrieve()
+			.onStatus(HttpStatusCode::is4xxClientError, (request, response) -> {
+				throw new RuntimeException("Client error, code: " + response.getStatusCode());
+			})
+			.onStatus(HttpStatusCode::is5xxServerError, (request, response) -> {
+				throw new RuntimeException("Server error, code: " + response.getStatusCode());
+			})
+			.body(String.class);
+	}
+
+	public <T> String post(String uri, Map<String, Object> variables, T value) {
+		try {
+			return restClient.post()
+				.uri(uriBuilder -> uriBuilder.path(uri).build(variables))
+				.contentType(MediaType.APPLICATION_JSON)
+				.body(jsonParseService.objectToJson(value))
+				.retrieve()
+				.onStatus(HttpStatusCode::is4xxClientError, (request, response) -> {
+					throw new RuntimeException("Client error, code: " + response.getStatusCode());
+				})
+				.onStatus(HttpStatusCode::is5xxServerError, (request, response) -> {
+					throw new RuntimeException("Server error, code: " + response.getStatusCode());
+				})
+				.body(String.class);
+		}
+		catch (JsonProcessingException e) {
+			throw new RuntimeException("Serialization failed", e);
+		}
+	}
+
+}
