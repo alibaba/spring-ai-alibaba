d859be2a75a86c80dd788b5ee0f0201cd3764e81
VLSMB <2047857654@qq.com>
2025-04-13 15:21:45 +0800

    fix: rename tool-call/commontoolcall moudule to tool-call/common

---
+++ community/tool-calls/spring-ai-alibaba-starter-tool-calling-common/src/main/java/com/alibaba/cloud/ai/toolcalling/common/CommonToolCallConstants.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2024-2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.alibaba.cloud.ai.toolcalling.common;
+
+/**
+ * @author vlsmb
+ */
+public class CommonToolCallConstants {
+
+	public static final String TOOLCALLING_CONFIG_PREFIX = "spring.ai.alibaba.toolcalling";
+
+	public static final String DEFAULT_BASE_URL = "/";
+
+	public static final Integer DEFAULT_NETWORK_TIMEOUT = 10;
+
+}
