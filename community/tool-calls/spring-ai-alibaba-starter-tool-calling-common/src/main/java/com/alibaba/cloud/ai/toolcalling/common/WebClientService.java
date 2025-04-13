25e353ce5bc1aa11286885c8706382d7aa9d6b5f
VLSMB <2047857654@qq.com>
2025-04-13 15:21:45 +0800

    fix: rename tool-call/commontoolcall moudule to tool-call/common

--- community/tool-calls/spring-ai-alibaba-starter-tool-calling-common/src/main/java/com/alibaba/cloud/ai/toolcalling/common/WebClientService.java
+++ community/tool-calls/spring-ai-alibaba-starter-tool-calling-common/src/main/java/com/alibaba/cloud/ai/toolcalling/common/WebClientService.java
@@ -1,4 +1,19 @@
-package com.alibaba.cloud.ai.toolcalling.commontoolcall;
+/*
+ * Copyright 2024-2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.alibaba.cloud.ai.toolcalling.common;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.http.HttpHeaders;
@@ -11,21 +26,31 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Consumer;
 
+/**
+ * @author vlsmb
+ */
 public class WebClientService {
 
 	private final WebClient webClient;
 
 	private final JsonParseService jsonParseService;
 
-	public WebClientService(JsonParseService jsonParseService) {
+	private final CommonToolCallProperties properties;
+
+	public WebClientService(JsonParseService jsonParseService, CommonToolCallProperties properties) {
 		this.jsonParseService = jsonParseService;
+		this.properties = properties;
 		this.webClient = WebClient.builder().build();
 	}
 
-	public WebClientService(String baseUrl, Consumer<HttpHeaders> httpHeadersConsumer,
+	public WebClientService(Consumer<HttpHeaders> httpHeadersConsumer, CommonToolCallProperties properties,
 			JsonParseService jsonParseService) {
-		this.webClient = WebClient.builder().baseUrl(baseUrl).defaultHeaders(httpHeadersConsumer).build();
+		this.webClient = WebClient.builder()
+			.baseUrl(properties.getBaseUrl())
+			.defaultHeaders(httpHeadersConsumer)
+			.build();
 		this.jsonParseService = jsonParseService;
+		this.properties = properties;
 	}
 
 	public Mono<String> get(String uri, Map<String, Object> variables) {
