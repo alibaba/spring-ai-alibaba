d4f8af8e255ad299fd41e8cee1561e6bada0a717
NewGK <820193452@qq.com>
2025-04-13 19:19:32 +0800

    1. 新增 飞书表格 创建的逻辑

---
+++ community/tool-calls/spring-ai-alibaba-starter-tool-calling-larksuite/src/main/java/com/alibaba/cloud/ai/toolcalling/larksuite/param/resp/ValuesAppendResp.java
@@ -0,0 +1,9 @@
+package com.alibaba.cloud.ai.toolcalling.larksuite.param.resp;
+
+import com.lark.oapi.core.response.BaseResponse;
+
+/**
+ * @author NewGK
+ */
+public class ValuesAppendResp extends BaseResponse<ValuesAppendRespBody> {
+}
