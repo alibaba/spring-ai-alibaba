de90e9cc81483f1804a7717e48dca3ddc227117f
shiyiyue1102 <liuzunfei@gmail.com>
2025-04-14 15:54:15 +0800

    add spring-ai-alibaba-mcp nacos base

---
+++ spring-ai-alibaba-mcp/spring-ai-alibaba-mcp-nacos/src/main/java/com/alibaba/cloud/ai/mcp/nacos/NacosMcpRegistryAutoConfigration.java
@@ -0,0 +1,11 @@
+package com.alibaba.cloud.ai.mcp.nacos;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@EnableConfigurationProperties({
+		NacosMcpRegistryProperties.class
+})
+public class NacosMcpRegistryAutoConfigration {
+
+
+}
