53b5ac3e7bfd3e63babe06755e98484cb2563759
Sunrisea <1336317033@qq.com>
2025-04-17 19:21:37 +0800

    Support Mcp Server auto register to Nacos

--- spring-ai-alibaba-mcp/spring-ai-alibaba-mcp-nacos/src/main/java/com/alibaba/cloud/ai/mcp/nacos/NacosMcpRegistryAutoConfigration.java
+++ spring-ai-alibaba-mcp/spring-ai-alibaba-mcp-nacos/src/main/java/com/alibaba/cloud/ai/mcp/nacos/NacosMcpRegistryAutoConfigration.java
@@ -1,11 +1,49 @@
 package com.alibaba.cloud.ai.mcp.nacos;
 
+import io.modelcontextprotocol.server.McpAsyncServer;
+import io.modelcontextprotocol.server.McpSyncServer;
+import io.modelcontextprotocol.server.transport.StdioServerTransport;
+import io.modelcontextprotocol.spec.ServerMcpTransport;
+import org.springframework.ai.autoconfigure.mcp.server.McpServerProperties;
+import org.springframework.ai.autoconfigure.mcp.server.MpcServerAutoConfiguration;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
-@EnableConfigurationProperties({
-		NacosMcpRegistryProperties.class
-})
+/**
+ * @author Sunrisea
+ */
+@EnableConfigurationProperties({ NacosMcpRegistryProperties.class, McpServerProperties.class })
+@AutoConfiguration(after = MpcServerAutoConfiguration.class)
+@ConditionalOnProperty(prefix = McpServerProperties.CONFIG_PREFIX, name = "enabled", havingValue = "true",
+		matchIfMissing = true)
 public class NacosMcpRegistryAutoConfigration {
 
+	@Bean
+	@ConditionalOnBean(McpSyncServer.class)
+	public NacosMcpRegister nacosMcpRegisterSync(McpSyncServer mcpSyncServer,
+			NacosMcpRegistryProperties nacosMcpRegistryProperties, ServerMcpTransport mcpServerTransport) {
+		McpAsyncServer mcpAsyncServer = mcpSyncServer.getAsyncServer();
+		if (mcpServerTransport instanceof StdioServerTransport) {
+			return new NacosMcpRegister(mcpAsyncServer, nacosMcpRegistryProperties, "stdio");
+		}
+		else {
+			return new NacosMcpRegister(mcpAsyncServer, nacosMcpRegistryProperties, "sse");
+		}
+	}
+
+	@Bean
+	@ConditionalOnBean(McpAsyncServer.class)
+	public NacosMcpRegister nacosMcpRegisterAsync(McpAsyncServer mcpAsyncServer,
+			NacosMcpRegistryProperties nacosMcpRegistryProperties, ServerMcpTransport mcpServerTransport) {
+		if (mcpServerTransport instanceof StdioServerTransport) {
+			return new NacosMcpRegister(mcpAsyncServer, nacosMcpRegistryProperties, "stdio");
+		}
+		else {
+			return new NacosMcpRegister(mcpAsyncServer, nacosMcpRegistryProperties, "sse");
+		}
+	}
 
 }
