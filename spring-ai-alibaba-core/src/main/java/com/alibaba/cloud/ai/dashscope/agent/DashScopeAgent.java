bd9be29e5a64fb5cc542b8abba88c5d5528621a0
Wangzy <wangziyuan455@Gmail.com>
2025-04-18 08:52:40 +0800

    feat: put all the DashScope constants in DashScopeApiConstants

--- spring-ai-alibaba-core/src/main/java/com/alibaba/cloud/ai/dashscope/agent/DashScopeAgent.java
+++ spring-ai-alibaba-core/src/main/java/com/alibaba/cloud/ai/dashscope/agent/DashScopeAgent.java
@@ -40,6 +40,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static com.alibaba.cloud.ai.dashscope.common.DashScopeApiConstants.*;
+
 /**
  * Title Dashscope low level agent.<br>
  * Description Dashscope low level agent.<br>
@@ -52,11 +54,7 @@ public final class DashScopeAgent extends Agent {
 
 	private static final Logger logger = LoggerFactory.getLogger(DashScopeAgent.class);
 
-	public static final String REQUEST_ID = "request_id";
 
-	public static final String USAGE = "usage";
-
-	public static final String OUTPUT = "output";
 
 	private final DashScopeAgentOptions options;
 
