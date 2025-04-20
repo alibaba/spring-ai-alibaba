54d9229b595a2d100a13850e3849c5559ebed3da
shenghang <shenghang@apache.org>
2025-04-20 17:55:17 +0800

    test: add unit tests for BaiduSearchService functionality

---
+++ community/tool-calls/spring-ai-alibaba-starter-tool-calling-baidusearch/src/test/java/com/alibaba/cloud/ai/toolcalling/baidusearch/BaiduSearchServiceTest.java
@@ -0,0 +1,22 @@
+package com.alibaba.cloud.ai.toolcalling.baidusearch;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class BaiduSearchServiceTest {
+
+    private final BaiduSearchService baiduSearchService = new BaiduSearchService();
+
+    @Test
+    void apply() {
+        BaiduSearchService.Request request = new BaiduSearchService.Request("Spring AI",10);
+        BaiduSearchService.Response apply = baiduSearchService.apply(request);
+        Assertions.assertNotNull(apply);
+        Assertions.assertEquals(10,apply.results().size());
+        apply.results().forEach(result -> {
+          Assertions.assertNotNull(result.title());
+          Assertions.assertNotNull(result.abstractText());
+        });
+    }
+}
