d36b0d1fb631be07cc95936f9889c5585d8428ee
kone-net <kone_net@163.com>
2025-04-25 23:27:47 +0800

    feat: toolcalling for larksuite sheet

--- community/tool-calls/spring-ai-alibaba-starter-tool-calling-larksuite/src/main/java/com/alibaba/cloud/ai/toolcalling/larksuite/param/req/ValueRange.java
+++ community/tool-calls/spring-ai-alibaba-starter-tool-calling-larksuite/src/main/java/com/alibaba/cloud/ai/toolcalling/larksuite/param/req/ValueRange.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2024-2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.alibaba.cloud.ai.toolcalling.larksuite.param.req;
 
 import com.google.gson.annotations.SerializedName;
@@ -9,58 +24,59 @@ import java.util.List;
  */
 public class ValueRange {
 
-    @SerializedName("range")
-    private String range;
+	@SerializedName("range")
+	private String range;
 
-    @SerializedName("values")
-    private List<List<String>> values;
+	@SerializedName("values")
+	private List<List<String>> values;
 
-    public String getRange() {
-        return range;
-    }
+	public String getRange() {
+		return range;
+	}
 
-    public void setRange(String range) {
-        this.range = range;
-    }
+	public void setRange(String range) {
+		this.range = range;
+	}
 
-    public List<List<String>> getValues() {
-        return values;
-    }
+	public List<List<String>> getValues() {
+		return values;
+	}
 
-    public void setValues(List<List<String>> values) {
-        this.values = values;
-    }
+	public void setValues(List<List<String>> values) {
+		this.values = values;
+	}
 
-    public ValueRange(Builder builder) {
-        this.range = builder.range;
-        this.values = builder.values;
-    }
+	public ValueRange(Builder builder) {
+		this.range = builder.range;
+		this.values = builder.values;
+	}
 
-    public static Builder newBuilder() {
-        return new Builder();
-    }
+	public static Builder newBuilder() {
+		return new Builder();
+	}
 
-    public static class Builder {
+	public static class Builder {
 
-        @SerializedName("range")
-        private String range;
+		@SerializedName("range")
+		private String range;
 
-        @SerializedName("values")
-        private List<List<String>> values;
+		@SerializedName("values")
+		private List<List<String>> values;
 
-        public Builder range(String range) {
-            this.range = range;
-            return this;
-        }
+		public Builder range(String range) {
+			this.range = range;
+			return this;
+		}
 
-        public Builder values(List<List<String>> values) {
-            this.values = values;
-            return this;
-        }
+		public Builder values(List<List<String>> values) {
+			this.values = values;
+			return this;
+		}
 
-        public ValueRange build() {
-            return new ValueRange(this);
-        }
+		public ValueRange build() {
+			return new ValueRange(this);
+		}
 
-    }
+	}
+
 }
--- community/tool-calls/spring-ai-alibaba-starter-tool-calling-larksuite/src/main/java/com/alibaba/cloud/ai/toolcalling/larksuite/param/resp/ValuesAppendRespBody.java
+++ community/tool-calls/spring-ai-alibaba-starter-tool-calling-larksuite/src/main/java/com/alibaba/cloud/ai/toolcalling/larksuite/param/resp/ValuesAppendRespBody.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2024-2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.alibaba.cloud.ai.toolcalling.larksuite.param.resp;
 
 import com.google.gson.annotations.SerializedName;
@@ -7,47 +22,48 @@ import com.google.gson.annotations.SerializedName;
  */
 public class ValuesAppendRespBody {
 
-    @SerializedName("revision")
-    private int revision;
+	@SerializedName("revision")
+	private int revision;
 
-    @SerializedName("spreadsheetToken")
-    private String spreadsheetToken;
+	@SerializedName("spreadsheetToken")
+	private String spreadsheetToken;
 
-    @SerializedName("tableRange")
-    private String tableRange;
+	@SerializedName("tableRange")
+	private String tableRange;
 
-    @SerializedName("updates")
-    private ValuesAppendRespBodyUpdates updates;
+	@SerializedName("updates")
+	private ValuesAppendRespBodyUpdates updates;
 
-    public int getRevision() {
-        return revision;
-    }
+	public int getRevision() {
+		return revision;
+	}
 
-    public void setRevision(int revision) {
-        this.revision = revision;
-    }
+	public void setRevision(int revision) {
+		this.revision = revision;
+	}
 
-    public String getSpreadsheetToken() {
-        return spreadsheetToken;
-    }
+	public String getSpreadsheetToken() {
+		return spreadsheetToken;
+	}
 
-    public void setSpreadsheetToken(String spreadsheetToken) {
-        this.spreadsheetToken = spreadsheetToken;
-    }
+	public void setSpreadsheetToken(String spreadsheetToken) {
+		this.spreadsheetToken = spreadsheetToken;
+	}
 
-    public String getTableRange() {
-        return tableRange;
-    }
+	public String getTableRange() {
+		return tableRange;
+	}
 
-    public void setTableRange(String tableRange) {
-        this.tableRange = tableRange;
-    }
+	public void setTableRange(String tableRange) {
+		this.tableRange = tableRange;
+	}
 
-    public ValuesAppendRespBodyUpdates getUpdates() {
-        return updates;
-    }
+	public ValuesAppendRespBodyUpdates getUpdates() {
+		return updates;
+	}
 
-    public void setUpdates(ValuesAppendRespBodyUpdates updates) {
-        this.updates = updates;
-    }
+	public void setUpdates(ValuesAppendRespBodyUpdates updates) {
+		this.updates = updates;
+	}
+
 }
