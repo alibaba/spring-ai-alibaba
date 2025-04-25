1abc036169dcd5ac527bc04f429d0166302938bf
kone-net <kone_net@163.com>
2025-04-25 23:27:47 +0800

    feat: toolcalling for larksuite sheet

--- community/tool-calls/spring-ai-alibaba-starter-tool-calling-larksuite/src/main/java/com/alibaba/cloud/ai/toolcalling/larksuite/LarkSuiteCreateSheetService.java
+++ community/tool-calls/spring-ai-alibaba-starter-tool-calling-larksuite/src/main/java/com/alibaba/cloud/ai/toolcalling/larksuite/LarkSuiteCreateSheetService.java
@@ -31,20 +31,28 @@ import com.lark.oapi.service.drive.v1.enums.BaseMemberPermEnum;
 import com.lark.oapi.service.drive.v1.model.BaseMember;
 import com.lark.oapi.service.drive.v1.model.CreatePermissionMemberReq;
 import com.lark.oapi.service.drive.v1.model.CreatePermissionMemberResp;
-import com.lark.oapi.service.sheets.v3.model.*;
+import com.lark.oapi.service.sheets.v3.model.CreateSpreadsheetReq;
+import com.lark.oapi.service.sheets.v3.model.CreateSpreadsheetResp;
+import com.lark.oapi.service.sheets.v3.model.QuerySpreadsheetSheetReq;
+import com.lark.oapi.service.sheets.v3.model.QuerySpreadsheetSheetResp;
+import com.lark.oapi.service.sheets.v3.model.Spreadsheet;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.util.ObjectUtils;
 
-import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 import java.util.function.Function;
 
 /**
  * @author kone_net
  */
 public class LarkSuiteCreateSheetService implements Function<LarkSuiteCreateSheetService.SheetRequest, Object> {
+
+	private static final String RANGE_FORMAT = "%s!A1:%s%d";
+
+	private static final String SHEET = "sheet";
+
+	private static final String LARK_SHEET_URL = "/open-apis/sheets/v2/spreadsheets/%s/values_append?insertDataOption=OVERWRITE";
 
 	private static final Logger logger = LoggerFactory.getLogger(LarkSuiteCreateSheetService.class);
 
@@ -67,8 +75,8 @@ public class LarkSuiteCreateSheetService implements Function<LarkSuiteCreateShee
 			throw new IllegalArgumentException("current larksuite appId or appSecret must not be null.");
 		}
 
-		logger.debug("current larksuite.appId is {},appSecret is {}",
-				larkSuiteProperties.getAppId(), larkSuiteProperties.getAppSecret());
+		logger.debug("current larksuite.appId is {},appSecret is {}", larkSuiteProperties.getAppId(),
+				larkSuiteProperties.getAppSecret());
 
 		Client client = Client.newBuilder(larkSuiteProperties.getAppId(), larkSuiteProperties.getAppSecret()).build();
 
@@ -87,19 +95,20 @@ public class LarkSuiteCreateSheetService implements Function<LarkSuiteCreateShee
 		}
 
 		Spreadsheet finalSpreadsheet = spreadsheet;
-        new Thread(() -> {
-			try {
-				// 获取 sheetId
-				String sheetId = getSheetId(client, finalSpreadsheet.getSpreadsheetToken());
-				// 追加数据
-				ValuesAppendReq valuesAppendReq = buildWriteRequest(finalSpreadsheet.getSpreadsheetToken(), sheetId, request.data());
-				valuesAppend(client, valuesAppendReq);
-				// 分配权限
-				addPermission(client, finalSpreadsheet.getSpreadsheetToken(), request.email());
-			} catch (Exception e) {
-                throw new RuntimeException(e);
-            }
-        }).start();
+		try {
+			// 获取 sheetId
+			String sheetId = getSheetId(client, finalSpreadsheet.getSpreadsheetToken());
+			// 追加数据
+			ValuesAppendReq valuesAppendReq = buildWriteRequest(finalSpreadsheet.getSpreadsheetToken(), sheetId,
+					request.data());
+			valuesAppend(client, valuesAppendReq);
+			// 分配权限
+			addPermission(client, finalSpreadsheet.getSpreadsheetToken(), request.email());
+		}
+		catch (Exception e) {
+			logger.error("failed to create larksuite sheet, caused by: ", e);
+			throw new RuntimeException(e);
+		}
 
 		return spreadsheet.getUrl();
 	}
@@ -108,27 +117,24 @@ public class LarkSuiteCreateSheetService implements Function<LarkSuiteCreateShee
 			@JsonProperty(required = true,
 					value = "title") @JsonPropertyDescription("the larksuite sheet title") String title,
 			@JsonProperty(required = true,
-					value = "email") @JsonPropertyDescription("email that needs to be authorized for the user")
-			String email,
+					value = "email") @JsonPropertyDescription("email that needs to be authorized for the user") String email,
 			@JsonProperty(required = true,
 					value = "data") @JsonPropertyDescription("the larksuite sheet data") List<List<String>> data) {
 	}
 
 	/**
 	 * 创建 飞书表格
-	 *
-	 * @param title			飞书表格标题
-	 * @return				飞书表格链接
-	 * @throws Exception	抛出异常
+	 * @param title 飞书表格标题
+	 * @return 飞书表格链接
+	 * @throws Exception 抛出异常
 	 */
-    private Spreadsheet createSpreadsheet(String title) throws Exception {
+	private Spreadsheet createSpreadsheet(String title) throws Exception {
 		CreateSpreadsheetReq req = CreateSpreadsheetReq.newBuilder()
-				.spreadsheet(Spreadsheet.newBuilder()
-						.title(title)
-						.build())
-				.build();
+			.spreadsheet(Spreadsheet.newBuilder().title(title).build())
+			.build();
 
-		Client larkClient = Client.newBuilder(larkSuiteProperties.getAppId(), larkSuiteProperties.getAppSecret()).build();
+		Client larkClient = Client.newBuilder(larkSuiteProperties.getAppId(), larkSuiteProperties.getAppSecret())
+			.build();
 
 		CreateSpreadsheetResp resp = larkClient.sheets().v3().spreadsheet().create(req);
 		return resp.getData().getSpreadsheet();
@@ -136,56 +142,46 @@ public class LarkSuiteCreateSheetService implements Function<LarkSuiteCreateShee
 
 	/**
 	 * 获取 Sheet1 的 sheet ID
-	 *
-	 * @param larkClient			飞书客户端
-	 * @param spreadsheetToken		飞书表格 token
-	 * @return						飞书表格 ID
-	 * @throws Exception			抛出异常
+	 * @param larkClient 飞书客户端
+	 * @param spreadsheetToken 飞书表格 token
+	 * @return 飞书表格 ID
+	 * @throws Exception 抛出异常
 	 */
 	private String getSheetId(Client larkClient, String spreadsheetToken) throws Exception {
 		// 查询 电子表格 的 sheet 信息
-		QuerySpreadsheetSheetReq req = QuerySpreadsheetSheetReq.newBuilder()
-				.spreadsheetToken(spreadsheetToken)
-				.build();
+		QuerySpreadsheetSheetReq req = QuerySpreadsheetSheetReq.newBuilder().spreadsheetToken(spreadsheetToken).build();
 
 		QuerySpreadsheetSheetResp resp = larkClient.sheets().v3().spreadsheetSheet().query(req);
+		if (null == resp || null == resp.getData() || null == resp.getData().getSheets()
+				|| resp.getData().getSheets().length == 0) {
+			throw new RuntimeException("get sheet id is empty");
+		}
 
-		Optional<Sheet> existingSheet = Arrays.stream(resp.getData().getSheets())
-				.filter(sheet -> "Sheet1".equals(sheet.getTitle()))
-				.findFirst();
-
-        return existingSheet.map(Sheet::getSheetId).orElse(null);
-    }
+		return resp.getData().getSheets()[0].getSheetId();
+	}
 
 	/**
 	 * 构造 追加数据 请求
-	 *
-	 * @param spreadsheetToken		飞书表格 token
-	 * @param sheetId				sheet ID
-	 * @param sheetData				追加数据
-	 * @return						追加数据请求
+	 * @param spreadsheetToken 飞书表格 token
+	 * @param sheetId sheet ID
+	 * @param sheetData 追加数据
+	 * @return 追加数据请求
 	 */
 	private ValuesAppendReq buildWriteRequest(String spreadsheetToken, String sheetId, List<List<String>> sheetData) {
 		int rowCount = sheetData.size();
 		int colCount = rowCount > 0 ? sheetData.get(0).size() : 0;
 
 		return ValuesAppendReq.newBuilder()
-				.spreadsheetToken(spreadsheetToken)
-				.body(ValuesAppendReqBody.newBuilder()
-						.valueRange(ValueRange.newBuilder()
-								.range(getRange(sheetId, rowCount, colCount))
-								.values(sheetData)
-								.build())
-						.build())
-				.build();
+			.spreadsheetToken(spreadsheetToken)
+			.body(ValuesAppendReqBody.newBuilder()
+				.valueRange(
+						ValueRange.newBuilder().range(getRange(sheetId, rowCount, colCount)).values(sheetData).build())
+				.build())
+			.build();
 	}
 
 	private String getRange(String sheetId, int rowCount, int colCount) {
-		return String.format("%s!A1:%s%d",
-				sheetId,
-				toColumnName(colCount),
-				rowCount
-				);
+		return String.format(RANGE_FORMAT, sheetId, toColumnName(colCount), rowCount);
 	}
 
 	private String toColumnName(int colNum) {
@@ -200,23 +196,18 @@ public class LarkSuiteCreateSheetService implements Function<LarkSuiteCreateShee
 
 	/**
 	 * 向飞书表格追加数据
-	 *
-	 * @param larkClient		飞书客户端
-	 * @param req				追加数据请求
-	 * @throws Exception		抛出异常
+	 * @param larkClient 飞书客户端
+	 * @param req 追加数据请求
+	 * @throws Exception 抛出异常
 	 */
 	private void valuesAppend(Client larkClient, ValuesAppendReq req) throws Exception {
 		// 请求参数选项
 		RequestOptions reqOptions = new RequestOptions();
 
 		// 发起请求
-		RawResponse httpResponse =  larkClient.post(
-				String.format("/open-apis/sheets/v2/spreadsheets/%s/values_append?insertDataOption=OVERWRITE", req.getSpreadsheetToken()),
-				req.getBody(),
-				AccessTokenType.Tenant, reqOptions
-		);
+		RawResponse httpResponse = larkClient.post(String.format(LARK_SHEET_URL, req.getSpreadsheetToken()),
+				req.getBody(), AccessTokenType.Tenant, reqOptions);
 
-		// 反序列化
 		ValuesAppendResp resp = UnmarshalRespUtil.unmarshalResp(httpResponse, ValuesAppendResp.class);
 		if (resp == null) {
 			throw new IllegalArgumentException("The result returned by the server is illegal");
@@ -232,23 +223,22 @@ public class LarkSuiteCreateSheetService implements Function<LarkSuiteCreateShee
 
 	/**
 	 * 分配飞书表格的授权
-	 *
-	 * @param larkClient			飞书客户端
-	 * @param spreadsheetToken		飞书表格 token
-	 * @param email					分配权限的邮箱
-	 * @throws Exception			抛出异常
+	 * @param larkClient 飞书客户端
+	 * @param spreadsheetToken 飞书表格 token
+	 * @param email 分配权限的邮箱
+	 * @throws Exception 抛出异常
 	 */
 	private void addPermission(Client larkClient, String spreadsheetToken, String email) throws Exception {
 		CreatePermissionMemberReq req = CreatePermissionMemberReq.newBuilder()
-				.token(spreadsheetToken)
-				.type("sheet")
-				.baseMember(BaseMember.newBuilder()
-						.memberId(email)
-						.memberType(BaseMemberMemberTypeEnum.EMAIL)
-						.perm(BaseMemberPermEnum.FULL_ACCESS)
-						.build())
-				.needNotification(true)
-				.build();
+			.token(spreadsheetToken)
+			.type(SHEET)
+			.baseMember(BaseMember.newBuilder()
+				.memberId(email)
+				.memberType(BaseMemberMemberTypeEnum.EMAIL)
+				.perm(BaseMemberPermEnum.FULL_ACCESS)
+				.build())
+			.needNotification(true)
+			.build();
 
 		CreatePermissionMemberResp resp = larkClient.drive().v1().permissionMember().create(req);
 
