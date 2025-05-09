90ea0bd14ea992ac3590588103791d90af2cc707
yuluo-yx <yuluo08290126@gmail.com>
2025-05-09 21:34:52 +0800

    fix
    Signed-off-by: yuluo-yx <yuluo08290126@gmail.com>

--- tools/make/java.mk
+++ tools/make/java.mk
@@ -36,11 +36,6 @@ format-check: ## Format Check the code
 	mvn spring-javaformat:validate
 
 .PHONY: checkstyle-check
-checkstyle-check: ## Checkstyle Check the code
-	@$(LOG_TARGET)
-	mvn validate -Ddisable.checks=false
-
-.PHONY: checkstyle-check
 checkstyle-check: ## Checkstyle Check the code and output to target/checkstyle-report.xml
 	@$(LOG_TARGET)
 	mvn clean compile -Dcheckstyle.skip=false -Dcheckstyle.output.file=checkstyle-report.xml checkstyle:checkstyle
