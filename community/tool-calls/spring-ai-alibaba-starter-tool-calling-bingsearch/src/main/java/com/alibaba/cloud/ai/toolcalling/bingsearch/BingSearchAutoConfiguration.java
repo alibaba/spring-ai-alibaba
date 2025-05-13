f74b2aa4efdacc28b6bda99a51d301f27efd07f7
VLSMB <2047857654@qq.com>
2025-05-13 14:22:07 +0800

    fix: format tool-calls

--- community/tool-calls/spring-ai-alibaba-starter-tool-calling-bingsearch/src/main/java/com/alibaba/cloud/ai/toolcalling/bingsearch/BingSearchAutoConfiguration.java
+++ community/tool-calls/spring-ai-alibaba-starter-tool-calling-bingsearch/src/main/java/com/alibaba/cloud/ai/toolcalling/bingsearch/BingSearchAutoConfiguration.java
@@ -44,8 +44,8 @@ public class BingSearchAutoConfiguration {
 				WebClientTool.builder(jsonParseTool, properties).httpHeadersConsumer(httpHeaders -> {
 					httpHeaders.add(HttpHeaders.USER_AGENT,
 							"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36");
-					if (!StringUtils.hasText(properties.getToken())) {
-						throw new IllegalArgumentException("token is empty");
+					if (!StringUtils.hasText(properties.getToken()) || properties.getToken().length() != 32) {
+						throw new IllegalArgumentException("token is empty or invalid");
 					}
 					httpHeaders.add(BingSearchProperties.OCP_APIM_SUBSCRIPTION_KEY, properties.getToken());
 				}).build(), properties, jsonParseTool);
