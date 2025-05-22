aec9048f4abfe5b502365143a05a27de40a32c72
Yeaury <3144550951@qq.com>
2025-05-22 11:47:57 +0800

    Refactoring githubtoolkit module

--- community/tool-calls/spring-ai-alibaba-starter-tool-calling-githubtoolkit/src/main/java/com/alibaba/cloud/ai/toolcalling/githubtoolkit/GithubToolKitProperties.java
+++ community/tool-calls/spring-ai-alibaba-starter-tool-calling-githubtoolkit/src/main/java/com/alibaba/cloud/ai/toolcalling/githubtoolkit/GithubToolKitProperties.java
@@ -15,28 +15,27 @@
  */
 package com.alibaba.cloud.ai.toolcalling.githubtoolkit;
 
+import com.alibaba.cloud.ai.toolcalling.common.CommonToolCallProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import static com.alibaba.cloud.ai.toolcalling.common.CommonToolCallConstants.TOOL_CALLING_CONFIG_PREFIX;
 
 /**
  * @author Yeaury
  */
-@ConfigurationProperties(prefix = "spring.ai.alibaba.toolcalling.github")
-public class GithubToolKitProperties {
+@ConfigurationProperties(prefix = GithubToolKitProperties.GITHUB_TOOLKIT_PREFIX)
+public class GithubToolKitProperties extends CommonToolCallProperties {
+
+	protected static final String GITHUB_TOOLKIT_PREFIX = TOOL_CALLING_CONFIG_PREFIX + ".githubtoolkit";
 
 	public static final String X_GitHub_Api_Version = "2022-11-28";
-
-	private String token;
 
 	private String owner;
 
 	private String repository;
 
-	public String getToken() {
-		return token;
-	}
-
-	public void setToken(String token) {
-		this.token = token;
+	public GithubToolKitProperties() {
+		super("https://api.github.com");
 	}
 
 	public String getOwner() {
@@ -53,6 +52,22 @@ public class GithubToolKitProperties {
 
 	public void setRepository(String repository) {
 		this.repository = repository;
+	}
+
+	/**
+	 * @deprecated Use {@link CommonToolCallProperties#getToken()} instead
+	 */
+	@Deprecated
+	public String getToken() {
+		return super.getToken();
+	}
+
+	/**
+	 * @deprecated Use {@link CommonToolCallProperties#setToken(String)} instead
+	 */
+	@Deprecated
+	public void setToken(String token) {
+		super.setToken(token);
 	}
 
 }
