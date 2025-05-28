75e3bcd63a8718ee6c58d15e5f8e86c26e0a092b
yingzi <GTyingzi@gmail.com>
2025-05-28 16:12:37 +0800

    format

--- spring-ai-alibaba-deepresearch/src/main/java/com/alibaba/cloud/ai/example/deepresearch/controller/DeepResearchController.java
+++ spring-ai-alibaba-deepresearch/src/main/java/com/alibaba/cloud/ai/example/deepresearch/controller/DeepResearchController.java
@@ -44,104 +44,104 @@ import reactor.core.publisher.Flux;
 @RequestMapping("/deep-research")
 public class DeepResearchController {
 
-    private static final Logger logger = LoggerFactory.getLogger(DeepResearchController.class);
+	private static final Logger logger = LoggerFactory.getLogger(DeepResearchController.class);
 
-    private final CompiledGraph compiledGraph;
+	private final CompiledGraph compiledGraph;
 
-    @Autowired
-    public DeepResearchController(@Qualifier("deepResearch") StateGraph stateGraph) throws GraphStateException {
-        this.compiledGraph = stateGraph.compile();
-    }
+	@Autowired
+	public DeepResearchController(@Qualifier("deepResearch") StateGraph stateGraph) throws GraphStateException {
+		this.compiledGraph = stateGraph.compile();
+	}
 
-    @PostMapping("/chat/stream")
-    public Flux<Map<String, Object>> chatStream(@RequestBody ChatRequest chatRequest) {
-        RunnableConfig runnableConfig = RunnableConfig.builder()
-                .threadId(String.valueOf(chatRequest.threadId()))
-                .build();
-        Map<String, Object> objectMap = new HashMap<>();
-        // 人类反馈消息
-        if (!chatRequest.autoAcceptPlan() && StringUtils.hasText(chatRequest.interruptFeedback())) {
-            objectMap.put("feed_back", chatRequest.interruptFeedback());
-            StateSnapshot stateSnapshot = compiledGraph.getState(runnableConfig);
-            OverAllState state = stateSnapshot.state();
-            state.withResume();
-            state.withHumanFeedback(new OverAllState.HumanFeedback(objectMap, "research_team"));
-            AsyncGenerator<NodeOutput> resultFuture = compiledGraph.stream(state, runnableConfig);
-            Stream<OverAllState> overAllStateStream = resultFuture.stream().map(NodeOutput::state);
-            return Flux.fromStream(overAllStateStream).map(overAllState -> {
-                Map<String, Object> result = new HashMap<>();
-                result.put("thread_id", chatRequest.threadId());
-                result.put("data", overAllState.data());
-                return result;
-            });
-        }
-        // 首次提问
-        objectMap.put("thread_id", chatRequest.threadId());
-        objectMap.put("enable_background_investigation", chatRequest.enableBackgroundInvestigation());
-        objectMap.put("auto_accepted_plan", chatRequest.autoAcceptPlan());
-        objectMap.put("messages", chatRequest.messages());
-        objectMap.put("plan_iterations", 0);
-        objectMap.put("max_step_num", chatRequest.maxStepNum());
-        objectMap.put("current_plan", null);
-        objectMap.put("final_report", "");
-        logger.info("init inputs: {}", objectMap);
+	@PostMapping("/chat/stream")
+	public Flux<Map<String, Object>> chatStream(@RequestBody ChatRequest chatRequest) {
+		RunnableConfig runnableConfig = RunnableConfig.builder()
+			.threadId(String.valueOf(chatRequest.threadId()))
+			.build();
+		Map<String, Object> objectMap = new HashMap<>();
+		// 人类反馈消息
+		if (!chatRequest.autoAcceptPlan() && StringUtils.hasText(chatRequest.interruptFeedback())) {
+			objectMap.put("feed_back", chatRequest.interruptFeedback());
+			StateSnapshot stateSnapshot = compiledGraph.getState(runnableConfig);
+			OverAllState state = stateSnapshot.state();
+			state.withResume();
+			state.withHumanFeedback(new OverAllState.HumanFeedback(objectMap, "research_team"));
+			AsyncGenerator<NodeOutput> resultFuture = compiledGraph.stream(state, runnableConfig);
+			Stream<OverAllState> overAllStateStream = resultFuture.stream().map(NodeOutput::state);
+			return Flux.fromStream(overAllStateStream).map(overAllState -> {
+				Map<String, Object> result = new HashMap<>();
+				result.put("thread_id", chatRequest.threadId());
+				result.put("data", overAllState.data());
+				return result;
+			});
+		}
+		// 首次提问
+		objectMap.put("thread_id", chatRequest.threadId());
+		objectMap.put("enable_background_investigation", chatRequest.enableBackgroundInvestigation());
+		objectMap.put("auto_accepted_plan", chatRequest.autoAcceptPlan());
+		objectMap.put("messages", chatRequest.messages());
+		objectMap.put("plan_iterations", 0);
+		objectMap.put("max_step_num", chatRequest.maxStepNum());
+		objectMap.put("current_plan", null);
+		objectMap.put("final_report", "");
+		logger.info("init inputs: {}", objectMap);
 
-        objectMap.put("mcp_settings", chatRequest.mcpSettings());
-        Stream<OverAllState> overAllStateStream = compiledGraph.stream(objectMap, runnableConfig)
-                .stream()
-                .map(NodeOutput::state);
-        return Flux.fromStream(overAllStateStream).map(overAllState -> {
-            Map<String, Object> result = new HashMap<>();
-            result.put("thread_id", chatRequest.threadId());
-            result.put("data", overAllState.data());
-            return result;
-        });
-    }
+		objectMap.put("mcp_settings", chatRequest.mcpSettings());
+		Stream<OverAllState> overAllStateStream = compiledGraph.stream(objectMap, runnableConfig)
+			.stream()
+			.map(NodeOutput::state);
+		return Flux.fromStream(overAllStateStream).map(overAllState -> {
+			Map<String, Object> result = new HashMap<>();
+			result.put("thread_id", chatRequest.threadId());
+			result.put("data", overAllState.data());
+			return result;
+		});
+	}
 
-    @GetMapping("/chat")
-    public Map<String, Object> chat(@RequestParam(value = "query", defaultValue = "草莓蛋糕怎么做呀") String query,
-                                    @RequestParam(value = "enable_background_investigation",
-                                            defaultValue = "true") boolean enableBackgroundInvestigation,
-                                    @RequestParam(value = "auto_accepted_plan", defaultValue = "true") boolean autoAcceptedPlan,
-                                    @RequestParam(value = "thread_id", required = false, defaultValue = "0") Integer threadId) {
-        UserMessage userMessage = new UserMessage(query);
-        Map<String, Object> objectMap = Map.of("enable_background_investigation", enableBackgroundInvestigation,
-                "auto_accepted_plan", autoAcceptedPlan, "messages", List.of(userMessage));
+	@GetMapping("/chat")
+	public Map<String, Object> chat(@RequestParam(value = "query", defaultValue = "草莓蛋糕怎么做呀") String query,
+			@RequestParam(value = "enable_background_investigation",
+					defaultValue = "true") boolean enableBackgroundInvestigation,
+			@RequestParam(value = "auto_accepted_plan", defaultValue = "true") boolean autoAcceptedPlan,
+			@RequestParam(value = "thread_id", required = false, defaultValue = "0") Integer threadId) {
+		UserMessage userMessage = new UserMessage(query);
+		Map<String, Object> objectMap = Map.of("enable_background_investigation", enableBackgroundInvestigation,
+				"auto_accepted_plan", autoAcceptedPlan, "messages", List.of(userMessage));
 
-        if (threadId != 0) {
-            RunnableConfig runnableConfig = RunnableConfig.builder().threadId(String.valueOf(threadId)).build();
-            var resultFuture = compiledGraph.invoke(objectMap, runnableConfig);
-            return resultFuture.get().data();
-        } else {
-            var resultFuture = compiledGraph.invoke(objectMap);
-            return resultFuture.get().data();
-        }
-    }
+		if (threadId != 0) {
+			RunnableConfig runnableConfig = RunnableConfig.builder().threadId(String.valueOf(threadId)).build();
+			var resultFuture = compiledGraph.invoke(objectMap, runnableConfig);
+			return resultFuture.get().data();
+		}
+		else {
+			var resultFuture = compiledGraph.invoke(objectMap);
+			return resultFuture.get().data();
+		}
+	}
 
-    @GetMapping("/chat/resume")
-    public Map<String, Object> resume(@RequestParam(value = "thread_id", required = true) int threadId,
-                                      @RequestParam(value = "feed_back", required = true) String feedBack,
-                                      @RequestParam(value = "feed_back_content", required = false) String feedBackConent
-    ) {
-        RunnableConfig runnableConfig = RunnableConfig.builder().threadId(String.valueOf(threadId)).build();
-        Map<String, Object> objectMap = Map.of("feed_back", feedBack);
-        if ("n".equals(feedBack)) {
-            if (StringUtils.hasLength(feedBackConent)) {
-                objectMap.put("feed_back_conent", feedBackConent);
-            } else {
-                throw new RuntimeException("feed_back_content is required when feed_back is n");
-            }
-        }
+	@GetMapping("/chat/resume")
+	public Map<String, Object> resume(@RequestParam(value = "thread_id", required = true) int threadId,
+			@RequestParam(value = "feed_back", required = true) String feedBack,
+			@RequestParam(value = "feed_back_content", required = false) String feedBackConent) {
+		RunnableConfig runnableConfig = RunnableConfig.builder().threadId(String.valueOf(threadId)).build();
+		Map<String, Object> objectMap = Map.of("feed_back", feedBack);
+		if ("n".equals(feedBack)) {
+			if (StringUtils.hasLength(feedBackConent)) {
+				objectMap.put("feed_back_conent", feedBackConent);
+			}
+			else {
+				throw new RuntimeException("feed_back_content is required when feed_back is n");
+			}
+		}
 
+		StateSnapshot stateSnapshot = compiledGraph.getState(runnableConfig);
+		OverAllState state = stateSnapshot.state();
+		state.withResume();
+		state.withHumanFeedback(new OverAllState.HumanFeedback(objectMap, "research_team"));
 
-        StateSnapshot stateSnapshot = compiledGraph.getState(runnableConfig);
-        OverAllState state = stateSnapshot.state();
-        state.withResume();
-        state.withHumanFeedback(new OverAllState.HumanFeedback(objectMap, "research_team"));
+		var resultFuture = compiledGraph.invoke(objectMap, runnableConfig);
 
-        var resultFuture = compiledGraph.invoke(objectMap, runnableConfig);
-
-        return resultFuture.get().data();
-    }
+		return resultFuture.get().data();
+	}
 
 }
