83b68b34bbee1702dc58bb0e8e4ef911915ca31a
Yongping <fengluo97@foxmail.com>
2025-06-12 20:11:47 +0800

    feat: Feature/refactor time&jsonprocessor (#1000)
    * refactor: refactor tool-calls-time(#849)
    
    * refactor: refactor tool-calls-jsonprocessor(#849)
    
    * refactor: refactor tool-calls-jsonprocessor(#849)
    
    * refactor: refactor tool-calls-jsonprocessor(#849)
    
    * refactor(tool-calls): update AutoConfiguration(#849)
    
    * refactor(tool-calls): refactor-time&jsonprocessor(#849)
    
    ---------
    
    Co-authored-by: aias00 <liuhongyu@apache.org>
    Co-authored-by: shown <yuluo08290126@gmail.com>

--- community/tool-calls/spring-ai-alibaba-starter-tool-calling-time/src/main/java/com/alibaba/cloud/ai/toolcalling/time/TimeUtils.java
+++ community/tool-calls/spring-ai-alibaba-starter-tool-calling-time/src/main/java/com/alibaba/cloud/ai/toolcalling/time/TimeUtils.java
@@ -15,16 +15,31 @@
  */
 package com.alibaba.cloud.ai.toolcalling.time;
 
-import java.util.TimeZone;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 
 /**
  * @author chengle
  */
-public class GetCurrentLocalTimeService {
 
-	public String getCurrentLocalTime() {
-		TimeZone timeZone = TimeZone.getDefault();
-		return String.format("The current local time is %s", ZoneUtils.getTimeByZoneId(timeZone.getID()));
+public class TimeUtils {
+
+	public static String getTimeByZoneId(String zoneId) {
+
+		// Get the time zone using ZoneId
+		ZoneId zid = ZoneId.of(zoneId);
+
+		// Get the current time in this time zone
+		ZonedDateTime zonedDateTime = ZonedDateTime.now(zid);
+
+		// Defining a formatter
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss z");
+
+		// Format ZonedDateTime as a string
+		String formattedDateTime = zonedDateTime.format(formatter);
+
+		return formattedDateTime;
 	}
 
 }
