08ff6a5c5a1bff4fc725d1a2ad37a371ae0cecf5
vlooong <qinweilong0813@gmail.com>
2025-06-13 00:12:04 +0800

    feat(tool-calls): add Toutiao News unit test

---
+++ community/tool-calls/spring-ai-alibaba-starter-tool-calling-toutiaonews/src/test/java/com/alibaba/cloud/ai/toolcalling/toutiaonews/ToutiaoNewsSearchHotEventsTest.java
@@ -0,0 +1,27 @@
+package com.alibaba.cloud.ai.toolcalling.toutiaonews;
+
+import com.alibaba.cloud.ai.toolcalling.common.CommonToolCallAutoConfiguration;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.logging.Logger;
+
+@SpringBootTest(classes = {CommonToolCallAutoConfiguration.class, ToutiaoNewsAutoConfiguration.class})
+@DisplayName("Toutiao News Test")
+class ToutiaoNewsSearchHotEventsTest {
+    @Autowired
+    private ToutiaoNewsSearchHotEventsService toutiaoNewsSearchHotEventsService;
+
+    private static final Logger log = Logger.getLogger(ToutiaoNewsSearchHotEventsTest.class.getName());
+
+    @Test
+    @DisplayName("Tool-Calling Test")
+    public void testGetHotEventFromToutiaoNews() {
+        var resp = toutiaoNewsSearchHotEventsService.apply(new ToutiaoNewsSearchHotEventsService.Request());
+        assert resp != null && resp.events() != null;
+        log.info("results: " + resp.events());
+    }
+
+}
