4e006e025141ed7b2b75ceb1a7efb59b06911067
wangyuxuan <2602872398@qq.com>
2025-06-13 22:10:17 +0800

    add a LICENSE at the beginning

--- community/tool-calls/spring-ai-alibaba-starter-tool-calling-baidutranslate/src/test/java/com/alibaba/cloud/ai/toolcalling/baidutranslate/BaiduTranslateTest.java
+++ community/tool-calls/spring-ai-alibaba-starter-tool-calling-baidutranslate/src/test/java/com/alibaba/cloud/ai/toolcalling/baidutranslate/BaiduTranslateTest.java
@@ -1,5 +1,20 @@
-package com.alibaba.cloud.ai.toolcalling.baidutranslate;
+/*
+ * Copyright 2024-2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
+package com.alibaba.cloud.ai.toolcalling.baidutranslate;
 
 import com.alibaba.cloud.ai.toolcalling.common.CommonToolCallAutoConfiguration;
 import com.alibaba.cloud.ai.toolcalling.common.CommonToolCallConstants;
@@ -12,11 +27,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-
 /**
  * @author wang123494
  */
-
 @SpringBootTest(classes = { BaiduTranslateAutoConfiguration.class, CommonToolCallAutoConfiguration.class } )
 @DisplayName("BaiduTranslate Service Test")
 public class BaiduTranslateTest {
