14d10eb4118c98adc31b4e70788eda14a2bfe8d8
HY-love-sleep <73268470+HY-love-sleep@users.noreply.github.com>
2025-06-17 10:31:34 +0800

    feat(graph): graph studio add project properties (#1228)
    * feat(graph): add ApplicationYamlContributor
    
    * fix: BeanDefinitionOverrideException
    
    * chore: java format
    
    * fix: remove useless imports
    
    * feat(graph): impl GraphAppPropertiesCustomizer
    
    * chore: java format
    
    * chore: mvn check
    
    ---------
    
    Co-authored-by: aias00 <liuhongyu@apache.org>

--- spring-ai-alibaba-graph/spring-ai-alibaba-graph-studio/src/main/java/com/alibaba/cloud/ai/config/GraphInitializrConfiguration.java
+++ spring-ai-alibaba-graph/spring-ai-alibaba-graph-studio/src/main/java/com/alibaba/cloud/ai/config/GraphInitializrConfiguration.java
@@ -17,9 +17,11 @@ package com.alibaba.cloud.ai.config;
 
 import com.alibaba.cloud.ai.controller.GeneratorController;
 import com.alibaba.cloud.ai.format.EclipseJdtFormatProjectContributor;
+import com.alibaba.cloud.ai.service.generator.GraphAppPropertiesCustomizer;
 import com.alibaba.cloud.ai.service.generator.GraphProjectReqToDescConverter;
 import com.alibaba.cloud.ai.service.generator.GraphProjectRequest;
 import io.spring.initializr.generator.project.contributor.ProjectContributor;
+import io.spring.initializr.generator.spring.properties.ApplicationPropertiesCustomizer;
 import io.spring.initializr.metadata.InitializrMetadataProvider;
 import io.spring.initializr.web.controller.ProjectGenerationController;
 import io.spring.initializr.web.project.ProjectGenerationInvoker;
@@ -49,6 +51,11 @@ public class GraphInitializrConfiguration {
 	@Bean
 	public ProjectContributor eclipseJdtFormatContributor() {
 		return new EclipseJdtFormatProjectContributor();
+	}
+
+	@Bean
+	public ApplicationPropertiesCustomizer graphAppPropertiesCustomizer() {
+		return new GraphAppPropertiesCustomizer();
 	}
 
 }
