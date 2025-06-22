a5b0d2d53a24fba22aff15580467c91d0c559b63
shown.Ji <yuluo08290126@gmail.com>
2025-06-22 22:42:33 +0800

    fix
    Signed-off-by: shown.Ji <yuluo08290126@gmail.com>

--- tools/make/tools.mk
+++ tools/make/tools.mk
@@ -53,7 +53,7 @@ tools: ## Install ci tools
 
 	@echo "Installing mvnd"
 	curl -sL https://downloads.apache.org/maven/mvnd/${MVND_VERSION}/maven-mvnd-${MVND_VERSION}-linux-amd64.zip -o mvnd.zip \
-	unzip -q mvnd.zipAdd commentMore actions \
+	unzip -q mvnd.zip \
     mkdir -p $HOME/.local \
     mv maven-mvnd-${MVND_VERSION}-linux-amd64 /usr/local/bin/mvnd \
     mvnd --version
