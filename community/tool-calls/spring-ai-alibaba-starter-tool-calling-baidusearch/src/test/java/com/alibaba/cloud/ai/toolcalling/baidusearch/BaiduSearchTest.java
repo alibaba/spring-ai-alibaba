4a6595cbaee4fa5c94e9793ceb051144b8030977
VLSMB <2047857654@qq.com>
2025-06-24 14:29:46 +0800

    fix: skip baidu search test

--- community/tool-calls/spring-ai-alibaba-starter-tool-calling-baidusearch/src/test/java/com/alibaba/cloud/ai/toolcalling/baidusearch/BaiduSearchTest.java
+++ community/tool-calls/spring-ai-alibaba-starter-tool-calling-baidusearch/src/test/java/com/alibaba/cloud/ai/toolcalling/baidusearch/BaiduSearchTest.java
@@ -17,6 +17,7 @@ package com.alibaba.cloud.ai.toolcalling.baidusearch;
 
 import com.alibaba.cloud.ai.toolcalling.common.CommonToolCallAutoConfiguration;
 import com.alibaba.cloud.ai.toolcalling.common.interfaces.SearchService;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,6 +27,7 @@ import java.util.logging.Logger;
 
 @SpringBootTest(classes = { CommonToolCallAutoConfiguration.class, BaiduSearchAutoConfiguration.class })
 @DisplayName("Baidu Search Test")
+@Disabled("This service for directly scraping search web pages has been temporarily disabled due to intermittent parsing success rates; future implementation may transition to an API-based solution.")
 public class BaiduSearchTest {
 
 	@Autowired
