2961e499df6fe0266f85eaf793fac93f9cf46dc1
ViliamSun <sunweilin258@126.com>
2025-07-07 16:02:46 +0800

    feat(deepresearch): Added intelligent routing based on abstract nodes to support the first version

---
+++ spring-ai-alibaba-deepresearch/src/main/java/com/alibaba/cloud/ai/example/deepresearch/model/NodeDefinition.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on anfeat "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.alibaba.cloud.ai.example.deepresearch.model;
+
+/**
+ * Node Description information of the node
+ *
+ * @author ViliamSun
+ * @since 1.0.0
+ */
+public record NodeDefinition(
+		// The name of the node.
+		String name,
+		// The description of the node.
+		String description) {
+
+	public record SelectionNode(
+			// The reasoning behind the selection.
+			String reasoning,
+			// The name of the selected node.
+			String selection) {
+		@Override
+		public String toString() {
+			return String.format("{\"reasoning\": \"%s\", \"selection\": \"%s\"}", reasoning, selection);
+		}
+	}
+}
---
+++ spring-ai-alibaba-deepresearch/src/main/java/com/alibaba/cloud/ai/example/deepresearch/node/AbstractNode.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.alibaba.cloud.ai.example.deepresearch.node;
+
+import com.alibaba.cloud.ai.example.deepresearch.model.NodeDefinition;
+import com.alibaba.cloud.ai.example.deepresearch.util.NodeSelectionUtil;
+import org.springframework.ai.chat.client.ChatClient;
+
+/**
+ * Abstract class representing a node in a graph. It defines the structure for nodes with
+ * a method to get their definition.
+ *
+ * @author ViliamSun
+ * @since 1.0.0
+ */
+public abstract class AbstractNode {
+
+	private final ChatClient chatClient;
+
+	public AbstractNode(ChatClient.Builder builder) {
+		this.chatClient = builder.defaultSystem(String.format(GUIDE, NodeSelectionUtil.getAvailableNodes())).build();
+	}
+
+	/**
+	 * The hook function implemented by the child node obtains the description information
+	 * and name of the node
+	 * @return the NodeDefinition instance
+	 */
+	public abstract NodeDefinition definition();
+
+	private static final String GUIDE = """
+			Parse the input and select the most appropriate support node from the following options: %s First explain your rationale,
+			then provide your selection in JSON format:
+			\\{
+			 "reasoning": "Briefly explain why this ticket should be routed to a specific node. Consider key terms, user intent, and urgency level. ",
+			 "selection": "Selected node name"
+			\\}
+			""";
+
+	protected NodeDefinition.SelectionNode guide(String input) {
+		return chatClient.prompt().user(input).call().entity(NodeDefinition.SelectionNode.class);
+	}
+
+}
