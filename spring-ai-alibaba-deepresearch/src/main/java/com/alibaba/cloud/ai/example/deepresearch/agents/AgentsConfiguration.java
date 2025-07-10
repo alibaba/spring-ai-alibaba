eade04078624333d56bdd7a58779c3552085f7c4
sixiyida <2397735425@qq.com>
2025-07-10 23:55:45 +0800

    format

--- spring-ai-alibaba-deepresearch/src/main/java/com/alibaba/cloud/ai/example/deepresearch/agents/AgentsConfiguration.java
+++ spring-ai-alibaba-deepresearch/src/main/java/com/alibaba/cloud/ai/example/deepresearch/agents/AgentsConfiguration.java
@@ -51,7 +51,7 @@ public class AgentsConfiguration {
 
 	@Value("classpath:prompts/reflection.md")
 	private Resource reflectionPrompt;
- 
+
 	@Value("classpath:prompts/reporter.md")
 	private Resource reporterPrompt;
 
