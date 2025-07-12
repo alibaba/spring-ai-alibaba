4ab0d61c59b992f65f0ce6369f092f3c2ccec8ed
HunterPorter <zongpeng_hzp@163.com>
2025-07-12 11:47:39 +0800

    feat(tool-calls): add tencent map weather tool

---
+++ community/tool-calls/spring-ai-alibaba-starter-tool-calling-tencentmap/src/test/java/com/alibaba/cloud/ai/toolcalling/tencentmap/TencentMapWeatherServiceTest.java
@@ -0,0 +1,40 @@
+package com.alibaba.cloud.ai.toolcalling.tencentmap;
+
+import com.alibaba.cloud.ai.toolcalling.common.CommonToolCallAutoConfiguration;
+import com.alibaba.cloud.ai.toolcalling.common.CommonToolCallConstants;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.StringUtils;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author HunterPorter
+ */
+@SpringBootTest(classes = { TencentMapAutoConfiguration.class, CommonToolCallAutoConfiguration.class })
+@DisplayName("Tencent Map Weather Test")
+public class TencentMapWeatherServiceTest {
+
+	@Autowired
+	private TencentMapWeatherService weatherSearchService;
+
+	private static final Logger log = LoggerFactory.getLogger(TencentMapWeatherServiceTest.class);
+
+	@Test
+	@EnabledIfEnvironmentVariable(named = TencentMapConstants.API_KEY_ENV,
+			matches = CommonToolCallConstants.NOT_BLANK_REGEX)
+	@DisplayName("Tencent Map Tool-Calling Test")
+	public void testWeatherSearch() {
+		TencentMapWeatherService.Response resp = weatherSearchService
+			.apply(new TencentMapWeatherService.Request("北京天安门", null));
+		assert resp != null && StringUtils.hasText(resp.message());
+		log.info("Weather Search Response: {}", resp.message());
+		assertThat(resp.message()).doesNotContain("Error");
+	}
+
+}
