a39c13170fc5a7e1f18387fc652c49b289f73e7d
VLSMB <2047857654@qq.com>
2025-07-15 00:29:20 +0800

    fix: checkstyle

--- spring-ai-alibaba-nl2sql/spring-ai-alibaba-nl2sql-management/src/test/java/com/alibaba/cloud/ai/controller/DockerContainerPoolExecutorTest.java
+++ spring-ai-alibaba-nl2sql/spring-ai-alibaba-nl2sql-management/src/test/java/com/alibaba/cloud/ai/controller/DockerContainerPoolExecutorTest.java
@@ -25,8 +25,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-import java.util.List;
-import java.util.concurrent.Callable;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
