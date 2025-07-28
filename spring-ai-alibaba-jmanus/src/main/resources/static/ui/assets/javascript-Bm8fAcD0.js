bcf70c61ef3594c6e6cfb54c5381bc33165c0a8c
whisper <rainerWJY@gmail.com>
2025-07-28 10:22:27 +0800

    feat(jmanus): enhance index page (#1842)
    * fix(entity): update PlanExecutionRecord column definition to longtext for larger data storage
    
    * feat(i18n): update direct button text and add AI novel example
    
    * build front end
    
    ---------
    
    Co-authored-by: rainerWJY <answeropensource@alibabacloud.com>

---
+++ spring-ai-alibaba-jmanus/src/main/resources/static/ui/assets/javascript-Bm8fAcD0.js
@@ -0,0 +1,6 @@
+import{conf as t,language as e}from"./typescript-CfgPU9pV.js";import"./index-Dyywgn9d.js";import"./index-DO2yvTzX.js";import"./_plugin-vue_export-helper-w9WVOjBt.js";import"./useToast-Dk4yxrfJ.js";import"./index-VlvMYK3B.js";/*!-----------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Version: 0.45.0(5e5af013f8d295555a7210df0d5f2cea0bf5dd56)
+ * Released under the MIT license
+ * https://github.com/microsoft/monaco-editor/blob/main/LICENSE.txt
+ *-----------------------------------------------------------------------------*/var c=t,l={defaultToken:"invalid",tokenPostfix:".js",keywords:["break","case","catch","class","continue","const","constructor","debugger","default","delete","do","else","export","extends","false","finally","for","from","function","get","if","import","in","instanceof","let","new","null","return","set","static","super","switch","symbol","this","throw","true","try","typeof","undefined","var","void","while","with","yield","async","await","of"],typeKeywords:[],operators:e.operators,symbols:e.symbols,escapes:e.escapes,digits:e.digits,octaldigits:e.octaldigits,binarydigits:e.binarydigits,hexdigits:e.hexdigits,regexpctl:e.regexpctl,regexpesc:e.regexpesc,tokenizer:e.tokenizer};export{c as conf,l as language};
