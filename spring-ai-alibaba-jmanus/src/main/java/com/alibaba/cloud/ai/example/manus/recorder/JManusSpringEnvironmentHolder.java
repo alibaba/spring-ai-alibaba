eeaeed8f3e606914165bdc126a502c6a45a737fd
Albumen Kevin <jhq0812@gmail.com>
2025-07-31 20:50:30 +0800

    test

--- spring-ai-alibaba-jmanus/src/main/java/com/alibaba/cloud/ai/example/manus/recorder/JManusSpringEnvironmentHolder.java
+++ spring-ai-alibaba-jmanus/src/main/java/com/alibaba/cloud/ai/example/manus/recorder/JManusSpringEnvironmentHolder.java
@@ -21,7 +21,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class JManusSpringEnvironmentHolder implements EnvironmentAware {
-// 测试
+// 测试123
 	private static Environment environment;
 
 	public void setEnvironment(Environment environment) {
