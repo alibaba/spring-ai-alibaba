cec5997fb130137619754b3a243eb4d48f31e36e
Makoto <2762006003@qq.com>
2025-08-03 11:07:34 +0800

    remove autowired

--- spring-ai-alibaba-nl2sql/spring-ai-alibaba-nl2sql-management/src/main/java/com/alibaba/cloud/ai/controller/PromptConfigController.java
+++ spring-ai-alibaba-nl2sql/spring-ai-alibaba-nl2sql-management/src/main/java/com/alibaba/cloud/ai/controller/PromptConfigController.java
@@ -21,7 +21,6 @@ import com.alibaba.cloud.ai.entity.UserPromptConfig;
 import com.alibaba.cloud.ai.service.UserPromptConfigService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
