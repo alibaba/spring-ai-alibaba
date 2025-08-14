0f2e594feee6c57b287c98176dfd72a8be4cf866
Ken Liu <ken.lj.hz@gmail.com>
2025-08-14 11:17:19 +0800

    release(ci): 1.0.0.3 release (#2120)
    1.0.0.3 release

--- auto-configurations/spring-ai-alibaba-autoconfigure-memory/src/main/java/com/alibaba/cloud/ai/autoconfigure/memory/redis/RedisChatMemoryStandaloneConfiguration.java
+++ auto-configurations/spring-ai-alibaba-autoconfigure-memory/src/main/java/com/alibaba/cloud/ai/autoconfigure/memory/redis/RedisChatMemoryStandaloneConfiguration.java
@@ -19,7 +19,7 @@ package com.alibaba.cloud.ai.autoconfigure.memory.redis;
  * Configuration for Redis Memory using Redis Standalone
  *
  * @author benym
- * @date 2025/7/30 21:32
+ * @since 2025/7/30 21:32
  */
 public record RedisChatMemoryStandaloneConfiguration(String hostName, int port, String username, String password,
 		int timeout) {
