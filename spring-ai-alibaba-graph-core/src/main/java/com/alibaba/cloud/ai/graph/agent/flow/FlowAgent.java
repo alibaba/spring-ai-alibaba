efa5a85a23fdfa8294a2fcf0b4d227fd477b846e
liuhy <liuhongyu@apache.org>
2025-08-22 13:35:44 +0800

    feat(graph): Implement improved builder pattern for FlowAgent and its subclasses

--- spring-ai-alibaba-graph-core/src/main/java/com/alibaba/cloud/ai/graph/agent/flow/FlowAgent.java
+++ spring-ai-alibaba-graph-core/src/main/java/com/alibaba/cloud/ai/graph/agent/flow/FlowAgent.java
@@ -37,14 +37,14 @@ public abstract class FlowAgent extends BaseAgent {
 
 	protected KeyStrategyFactory keyStrategyFactory;
 
-	protected List<? extends BaseAgent> subAgents;
+	protected List<BaseAgent> subAgents;
 
 	protected StateGraph graph;
 
 	protected CompiledGraph compiledGraph;
 
 	protected FlowAgent(String name, String description, String outputKey, String inputKey,
-			KeyStrategyFactory keyStrategyFactory, CompileConfig compileConfig, List<? extends BaseAgent> subAgents)
+			KeyStrategyFactory keyStrategyFactory, CompileConfig compileConfig, List<BaseAgent> subAgents)
 			throws GraphStateException {
 		super(name, description, outputKey);
 		this.compileConfig = compileConfig;
@@ -67,8 +67,8 @@ public abstract class FlowAgent extends BaseAgent {
 		return graph;
 	}
 
-	protected abstract void processSubAgents(StateGraph graph, BaseAgent parentAgent,
-			List<? extends BaseAgent> subAgents) throws GraphStateException;
+	protected abstract void processSubAgents(StateGraph graph, BaseAgent parentAgent, List<BaseAgent> subAgents)
+			throws GraphStateException;
 
 	@Override
 	public AsyncNodeAction asAsyncNodeAction(String inputKeyFromParent, String outputKeyToParent)
@@ -102,7 +102,7 @@ public abstract class FlowAgent extends BaseAgent {
 		return keyStrategyFactory;
 	}
 
-	public List<? extends BaseAgent> subAgents() {
+	public List<BaseAgent> subAgents() {
 		return this.subAgents;
 	}
 
