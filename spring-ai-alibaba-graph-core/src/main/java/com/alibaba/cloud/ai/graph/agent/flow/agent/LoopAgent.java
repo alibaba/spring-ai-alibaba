77a55fef4d2dc408ba28b32d2d4d1677090f3447
VLSMB <2047857654@qq.com>
2025-08-27 06:15:31 +0800

    feat: simplify startNodeActionFunc and endNodeActionFunc

--- spring-ai-alibaba-graph-core/src/main/java/com/alibaba/cloud/ai/graph/agent/flow/agent/LoopAgent.java
+++ spring-ai-alibaba-graph-core/src/main/java/com/alibaba/cloud/ai/graph/agent/flow/agent/LoopAgent.java
@@ -38,7 +38,6 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.function.BiFunction;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
