acaa1c10a94e757334a3094b37f2e67e7b4ab8b6
xueht26 <13069167198@163.com>
2025-08-27 11:57:32 +0800

    fix: use single class import

--- spring-ai-alibaba-graph-core/src/main/java/com/alibaba/cloud/ai/graph/agent/a2a/A2aRemoteAgent.java
+++ spring-ai-alibaba-graph-core/src/main/java/com/alibaba/cloud/ai/graph/agent/a2a/A2aRemoteAgent.java
@@ -13,8 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.alibaba.cloud.ai.graph.agent.a2a;
 
+package com.alibaba.cloud.ai.graph.agent.a2a;
 import com.alibaba.cloud.ai.graph.*;
 import com.alibaba.cloud.ai.graph.action.AsyncNodeAction;
 import com.alibaba.cloud.ai.graph.action.NodeAction;
