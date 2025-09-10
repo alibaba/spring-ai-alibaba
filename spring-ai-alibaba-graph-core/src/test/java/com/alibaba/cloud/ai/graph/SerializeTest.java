dbbed9ce2b40aa450455e6b44bbb93aeeb701747
chickenlj <ken.lj.hz@gmail.com>
2025-09-10 09:03:21 +0800

    fix ut

--- spring-ai-alibaba-graph-core/src/test/java/com/alibaba/cloud/ai/graph/SerializeTest.java
+++ spring-ai-alibaba-graph-core/src/test/java/com/alibaba/cloud/ai/graph/SerializeTest.java
@@ -228,14 +228,14 @@ public class SerializeTest {
 		String json = serializer.getObjectMapper().writeValueAsString(output);
 
 		assertEquals(
-				"{\"@class\":\"com.alibaba.cloud.ai.graph.NodeOutput\",\"node\":\"node\",\"state\":null,\"subGraph\":true,\"end\":false,\"start\":false}",
+				"{\"@class\":\"com.alibaba.cloud.ai.graph.NodeOutput\",\"node\":\"node\",\"state\":null,\"subGraph\":true,\"start\":false,\"end\":false}",
 				json);
 
 		output.setSubGraph(false);
 		json = serializer.getObjectMapper().writeValueAsString(output);
 
 		assertEquals(
-				"{\"@class\":\"com.alibaba.cloud.ai.graph.NodeOutput\",\"node\":\"node\",\"state\":null,\"subGraph\":false,\"end\":false,\"start\":false}",
+				"{\"@class\":\"com.alibaba.cloud.ai.graph.NodeOutput\",\"node\":\"node\",\"state\":null,\"subGraph\":false,\"start\":false,\"end\":false}",
 				json);
 	}
 
