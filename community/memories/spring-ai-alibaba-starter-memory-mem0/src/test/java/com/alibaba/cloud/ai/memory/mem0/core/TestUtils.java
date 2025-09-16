dedc6aff6a4f3a1f1af97188fa15a5ce9f10ea62
yingzi <GTyingzi@gmail.com>
2025-09-17 01:05:18 +0800

    feat(mem0): adujst test

--- community/memories/spring-ai-alibaba-starter-memory-mem0/src/test/java/com/alibaba/cloud/ai/memory/mem0/core/TestUtils.java
+++ community/memories/spring-ai-alibaba-starter-memory-mem0/src/test/java/com/alibaba/cloud/ai/memory/mem0/core/TestUtils.java
@@ -1,18 +1,18 @@
-/// *
-// * Copyright 2024-2025 the original author or authors.
-// *
-// * Licensed under the Apache License, Version 2.0 (the "License");
-// * you may not use this file except in compliance with the License.
-// * You may obtain a copy of the License at
-// *
-// * https://www.apache.org/licenses/LICENSE-2.0
-// *
-// * Unless required by applicable law or agreed to in writing, software
-// * distributed under the License is distributed on an "AS IS" BASIS,
-// * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// * See the License for the specific language governing permissions and
-// * limitations under the License.
-// */
+/*
+ * Copyright 2024-2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.alibaba.cloud.ai.memory.mem0.core;
 
 import com.alibaba.cloud.ai.memory.mem0.advisor.Mem0ChatMemoryAdvisor;
@@ -27,12 +27,12 @@ import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
-/// **
-// * Test utility class providing common helper methods for testing
-// *
-// * @author Morain Miao
-// * @since 1.0.0
-// */
+/**
+ * Test utility class providing common helper methods for testing
+ *
+ * @author Morain Miao
+ * @since 1.0.0
+ */
 public class TestUtils {
 
 	public static Mem0Client createTestMem0Client() {
