1caa80fea3bc51d639d2630d3a0674ef21dd517a
Sunrisea <49605583+Sunrisea@users.noreply.github.com>
2025-09-19 09:43:45 +0800

    fix(studio): cherry-pick #2358, support observability of saa studio (#2447)

---
+++ spring-ai-alibaba-observation-extension/src/main/java/com/alibaba/cloud/ai/observation/constants/MetadataAttributes.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2024-2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.alibaba.cloud.ai.observation.constants;
+
+public final class MetadataAttributes {
+
+	public static final String PROMPT_KEY = "spring.ai.alibaba.prompt.key";
+
+	public static final String PROMPT_VERSION = "spring.ai.alibaba.prompt.version";
+
+	public static final String PROMPT_TEMPLATE = "spring.ai.alibaba.prompt.template";
+
+	public static final String PROMPT_VARIABLE = "spring.ai.alibaba.prompt.variable";
+
+	public static final String AGENT_NAME = "spring.ai.alibaba.agent.name";
+
+	public static final String AGENT_IP = "spring.ai.alibaba.agent.ip";
+
+	public static final String STUDIO_SOURCE = "spring.ai.alibaba.studio.source";
+
+}
