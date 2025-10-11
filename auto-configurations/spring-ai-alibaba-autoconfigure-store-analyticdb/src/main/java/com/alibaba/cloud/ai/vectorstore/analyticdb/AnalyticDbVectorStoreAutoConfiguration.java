6afd52666078d6e69f76f7f94311cecafc0acc14
SaladDay <1203511142@qq.com>
2025-10-11 12:43:58 +0000

    fix: add missing Conditional import

--- auto-configurations/spring-ai-alibaba-autoconfigure-store-analyticdb/src/main/java/com/alibaba/cloud/ai/vectorstore/analyticdb/AnalyticDbVectorStoreAutoConfiguration.java
+++ auto-configurations/spring-ai-alibaba-autoconfigure-store-analyticdb/src/main/java/com/alibaba/cloud/ai/vectorstore/analyticdb/AnalyticDbVectorStoreAutoConfiguration.java
@@ -32,6 +32,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.ConfigurationCondition;
 
 /**
