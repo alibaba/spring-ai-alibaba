ba51c7cee1826006c674a44d1e81d24e40ce1bdd
logic.wu0 <605524858@qq.com>
2025-10-25 00:40:15 +0800

    add Structured output

--- spring-ai-alibaba-studio/spring-ai-alibaba-studio-server/spring-ai-alibaba-studio-server-core/src/main/java/com/alibaba/cloud/ai/studio/core/structured/OutputMode.java
+++ spring-ai-alibaba-studio/spring-ai-alibaba-studio-server/spring-ai-alibaba-studio-server-core/src/main/java/com/alibaba/cloud/ai/studio/core/structured/OutputMode.java
@@ -1,7 +1,22 @@
+/*
+ * Copyright 2024-2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.alibaba.cloud.ai.studio.core.structured;
 
 public enum OutputMode {
-    NATIVE,    // 模型原生responseFormat
-    TOOL_CALL, // 工具调用方式
-    PROMPT     // Prompt工程方式
+    NATIVE,
+    TOOL_CALL,
+    PROMPT
 }
