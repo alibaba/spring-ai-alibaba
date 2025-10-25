398259724e322f3990cdaa783dfcba146d1947d3
VLSMB <2047857654@qq.com>
2025-10-25 22:59:42 +0800

    fix: add some documents

--- spring-ai-alibaba-agent-framework/src/main/java/com/alibaba/cloud/ai/graph/agent/tool/AbstractTool.java
+++ spring-ai-alibaba-agent-framework/src/main/java/com/alibaba/cloud/ai/graph/agent/tool/AbstractTool.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2024-2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.alibaba.cloud.ai.graph.agent.tool;
 
 import org.slf4j.Logger;
@@ -12,14 +28,28 @@ import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Consumer;
 
+/**
+ * 工具接口的抽象实现类
+ *
+ * @param <T> 输入参数类型
+ * @param <R> 输出参数类型
+ * @author vlsmb
+ * @since 2025/10/25
+ */
 public abstract class AbstractTool<T, R> implements BaseTool<T, R> {
 
     private static final Logger log = LoggerFactory.getLogger(AbstractTool.class);
 
     private final ExecutorService executor;
 
+    /**
+     * 存储工具调用的Entry
+     */
     private final ConcurrentHashMap<String, Entry<R>> entryMap = new ConcurrentHashMap<>();
 
+    /**
+     * 记录当前工具调用的状态
+     */
     private static class Entry<R> {
         AtomicBoolean cancelled = new AtomicBoolean(false);
         AtomicReference<Future<? extends R>> futureRef = new AtomicReference<>(null);
@@ -29,47 +59,68 @@ public abstract class AbstractTool<T, R> implements BaseTool<T, R> {
         this.executor = executor;
     }
 
+    /**
+     * 流式处理元素的Consumer（如果工具支持则需要重写本方法）
+     * @return Consumer
+     */
     protected Consumer<? super R> streamConsumer() {
-        return null;
+        return r -> {};
     }
 
+    /**
+     * 实现工具执行方法（用于生成ToolCallback）
+     * @param t 工具输入参数
+     * @param toolContext 工具上下文
+     * @return 输出参数
+     */
     @Override
     public R apply(T t, ToolContext toolContext) {
         String agentName = getAgentName(toolContext);
-        Entry<R> entry = entryMap.computeIfAbsent(agentName, k -> new Entry<>());
+        Entry<R> entry = entryMap.compute(agentName, (k, v) -> new Entry<>());
         try {
+            // 检测是否取消
             if(isCancelled(toolContext)) {
                 log.info("AgentName: {} , Tool cancelled", agentName);
                 return fallback(t, toolContext, new CancellationException("Tool cancelled"));
             }
+            // 提交任务到线程池
             Future<R> future = executor.submit(() -> {
                 try {
                     if(isCancelled(toolContext)) {
                         log.info("AgentName: {} , Tool cancelled", agentName);
                         throw new CancellationException("Tool cancelled");
                     }
+                    // 执行工具，使用子类提供的Consumer
                     return applyInterruptible(t, toolContext, streamConsumer());
                 } catch (InterruptedException e) {
                     log.info("AgentName: {} , Tool interrupted", agentName);
                     Thread.currentThread().interrupt();
                     return fallback(t, toolContext, e);
                 } catch (Exception e) {
+                    // 遇见异常，返回fallback结果
                     log.info("AgentName: {} , Tool Exception", agentName);
                     return fallback(t, toolContext, e);
                 }
             });
+            // 保存future到entry中，如果失败，取消当前任务
+            // （应该不会有同一个Agent同一时间调用同一个工具）
             if(!entry.futureRef.compareAndSet(null, future)) {
                 future.cancel(true);
                 log.info("AgentName: {} , Tool already applied", agentName);
                 throw new IllegalStateException("Tool already applied");
             }
+            // 检测是否取消
             if (isCancelled(toolContext)) {
                 return fallback(t, toolContext, new CancellationException("Tool cancelled"));
             }
+            // 返回任务结果
             return future.get();
         } catch (Exception e) {
+            // 遇见异常，返回fallback结果
+            log.info("AgentName: {} , Tool Exception", agentName);
             return fallback(t, toolContext, e);
         } finally {
+            // 移除当前entry
             entryMap.remove(agentName, entry);
         }
     }
@@ -83,6 +134,7 @@ public abstract class AbstractTool<T, R> implements BaseTool<T, R> {
             return;
         }
         if (entry.cancelled.compareAndSet(false, true)) {
+            // 如果entry中有future，则取消该future
             Future<? extends R> future = entry.futureRef.get();
             if (future != null && !future.isDone() && !future.isCancelled()) {
                 future.cancel(true);
--- spring-ai-alibaba-agent-framework/src/main/java/com/alibaba/cloud/ai/graph/agent/tool/BaseTool.java
+++ spring-ai-alibaba-agent-framework/src/main/java/com/alibaba/cloud/ai/graph/agent/tool/BaseTool.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2024-2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.alibaba.cloud.ai.graph.agent.tool;
 
 import org.springframework.ai.chat.model.ToolContext;
@@ -11,6 +27,8 @@ import java.util.function.Consumer;
  * 工具接口，定义工具方法，支持中断的方法，流式输出方法以及默认值返回方法。
  * ToolContext中必须要有agent_name的值，否则会报错
  *
+ * @param <T> 输入参数类型
+ * @param <R> 输出结果类型
  * @author vlsmb
  * @since 2025/10/25
  */
@@ -19,25 +37,25 @@ public interface BaseTool<T, R> extends BiFunction<T, ToolContext, R> {
     String AGENT_NAME = "agent_name";
 
     /**
-     * 工具方法，已经在AbstractTool中实现
+     * 工具方法，来自BiFunction，已经在AbstractTool中实现（用于定义toolCallback）
      * @param t 输入参数
      * @param toolContext 工具上下文
      * @return 输出结果
      */
     R apply(T t, ToolContext toolContext);
 
-    default R applyInterruptible(T t, ToolContext toolContext) throws Exception {
-        return applyInterruptible(t, toolContext, null);
-    }
-
     /**
      * 允许中断的apply方法，实现类需要需要实现此方法
      * @param t 输入参数
      * @param toolContext 工具上下文
-     * @param streamConsumer 每一个元素的Consumer（如果工具不支持流式可以置null）
+     * @param streamConsumer 每一个元素的Consumer（如果工具不支持流式可以置空）
      * @return 输出结果
      */
     R applyInterruptible(T t, ToolContext toolContext, Consumer<? super R> streamConsumer) throws Exception;
+
+    default R applyInterruptible(T t, ToolContext toolContext) throws Exception {
+        return applyInterruptible(t, toolContext, r -> {});
+    }
 
     /**
      * 返回包装后的ToolCallback
@@ -63,10 +81,24 @@ public interface BaseTool<T, R> extends BiFunction<T, ToolContext, R> {
      */
     R fallback(T t, ToolContext toolContext, Throwable throwable);
 
+    /**
+     * 取消正在调用的工具，立即返回fallback的值
+     * @param toolContext 工具上下文，需要有agent_name
+     */
     void cancel(ToolContext toolContext);
 
+    /**
+     * 判断工具是否被取消
+     * @param toolContext 工具上下文，需要有agent_name
+     * @return 是否被取消
+     */
     boolean isCancelled(ToolContext toolContext);
 
+    /**
+     * 获取工具名称，从工具上下文中获取agent_name的值
+     * @param toolContext 工具上下文
+     * @return 工具名称
+     */
     default String getAgentName(ToolContext toolContext) {
         Object obj = toolContext.getContext().get(AGENT_NAME);
         if(!(obj instanceof String agentName)) {
