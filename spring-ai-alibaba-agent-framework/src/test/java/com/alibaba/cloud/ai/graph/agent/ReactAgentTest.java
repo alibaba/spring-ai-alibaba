6e0a16d033d470bfc933279474512cc3a5bbc133
chickenlj <ken.lj.hz@gmail.com>
2025-11-22 08:06:46 +0800

    code format
    Change-Id: I40a0604e544b17d21484bd3983ceabe5804fd7c9

--- spring-ai-alibaba-agent-framework/src/test/java/com/alibaba/cloud/ai/graph/agent/ReactAgentTest.java
+++ spring-ai-alibaba-agent-framework/src/test/java/com/alibaba/cloud/ai/graph/agent/ReactAgentTest.java
@@ -32,11 +32,6 @@ import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.converter.BeanOutputConverter;
 import org.springframework.ai.converter.ListOutputConverter;
 import org.springframework.ai.converter.MapOutputConverter;
-import org.springframework.ai.converter.BeanOutputConverter;
-import org.springframework.ai.converter.ListOutputConverter;
-import org.springframework.ai.converter.MapOutputConverter;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.core.convert.support.DefaultConversionService;
 
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.core.convert.support.DefaultConversionService;
@@ -61,68 +56,6 @@ class ReactAgentTest {
 
 	private ChatModel chatModel;
 
-	// Inner class for outputType example
-	public static class PoemOutput {
-		private String title;
-		private String content;
-		private String style;
-
-		public String getTitle() {
-			return title;
-		}
-
-		public void setTitle(String title) {
-			this.title = title;
-		}
-
-		public String getContent() {
-			return content;
-		}
-
-		public void setContent(String content) {
-			this.content = content;
-		}
-
-		public String getStyle() {
-			return style;
-		}
-
-		public void setStyle(String style) {
-			this.style = style;
-		}
-
-		@Override
-		public String toString() {
-			return "PoemOutput{" +
-					"title='" + title + '\'' +
-					", content='" + content + '\'' +
-					", style='" + style + '\'' +
-					'}';
-		}
-	}
-
-	// Inner class for BeanOutputConverter example
-	public static class ActorsFilms {
-		private String actor;
-		private List<String> films;
-
-		public String getActor() {
-			return actor;
-		}
-
-		public void setActor(String actor) {
-			this.actor = actor;
-		}
-
-		public List<String> getFilms() {
-			return films;
-		}
-
-		public void setFilms(List<String> films) {
-			this.films = films;
-		}
-	}
-
 	@BeforeEach
 	void setUp() {
 		// Create DashScopeApi instance using the API key from environment variable
@@ -144,14 +77,16 @@ class ReactAgentTest {
 			assertTrue(result.isPresent(), "First result should be present");
 			OverAllState state1 = result.get();
 			assertTrue(state1.value("messages").isPresent(), "Messages should be present in first result");
-			assertEquals(2, ((List)state1.value("messages").get()).size(), "There should be 2 messages in the first result");
+			assertEquals(2, ((List) state1.value("messages")
+					.get()).size(), "There should be 2 messages in the first result");
 			Object messages1 = state1.value("messages").get();
 			assertNotNull(messages1, "Messages should not be null in first result");
 
 			assertTrue(result2.isPresent(), "Second result should be present");
 			OverAllState state2 = result2.get();
 			assertTrue(state2.value("messages").isPresent(), "Messages should be present in second result");
-			assertEquals(4, ((List<?>)state2.value("messages").get()).size(), "There should be 2 messages in the first result");
+			assertEquals(4, ((List<?>) state2.value("messages")
+					.get()).size(), "There should be 2 messages in the first result");
 			Object messages2 = state2.value("messages").get();
 			assertNotNull(messages2, "Messages should not be null in second result");
 
@@ -168,7 +103,7 @@ class ReactAgentTest {
 
 	@Test
 	public void testReactAgentMessage() throws Exception {
-		
+
 		ReactAgent agent = ReactAgent.builder().name("single_agent").model(chatModel).saver(new MemorySaver())
 				.build();
 		AssistantMessage message = agent.call("帮我写一篇100字左右散文。");
@@ -177,7 +112,7 @@ class ReactAgentTest {
 
 	@Test
 	public void testReactAgentWithOutputSchema() throws Exception {
-		
+
 
 		// Customized outputSchema
 		String customSchema = """
@@ -218,7 +153,7 @@ class ReactAgentTest {
 
 	@Test
 	public void testReactAgentWithOutputType() throws Exception {
-		
+
 
 		// outputType will be automatically convert to schema
 		ReactAgent agent = ReactAgent.builder()
@@ -235,13 +170,13 @@ class ReactAgentTest {
 		System.out.println(message.getText());
 
 		assertTrue(message.getText().contains("title") || message.getText().contains("content") ||
-				message.getText().contains("style"),
+						message.getText().contains("style"),
 				"Output should contain structured fields");
 	}
 
 	@Test
 	public void testReactAgentWithOutputSchemaAndInvoke() throws Exception {
-		
+
 
 		String jsonSchema = """
 				{
@@ -316,7 +251,7 @@ class ReactAgentTest {
 		Flux<NodeOutput> flux = agent.stream(new UserMessage("帮我写一篇100字左右散文。"));
 
 		flux.doOnNext(output -> {
-			if (output instanceof StreamingOutput<?> streamingOutput){
+			if (output instanceof StreamingOutput<?> streamingOutput) {
 				assertNotNull(streamingOutput, "NodeOutput should not be null");
 				assertNotNull(streamingOutput.tokenUsage(), "TokenUsage should not be null");
 				assertNotNull(streamingOutput.agent(), "Agent should not be null");
@@ -630,6 +565,68 @@ class ReactAgentTest {
 		System.out.println(message.getText());
 
 		assertTrue(message.getText().length() > 0, "Output should not be empty");
+	}
+
+	// Inner class for outputType example
+	public static class PoemOutput {
+		private String title;
+		private String content;
+		private String style;
+
+		public String getTitle() {
+			return title;
+		}
+
+		public void setTitle(String title) {
+			this.title = title;
+		}
+
+		public String getContent() {
+			return content;
+		}
+
+		public void setContent(String content) {
+			this.content = content;
+		}
+
+		public String getStyle() {
+			return style;
+		}
+
+		public void setStyle(String style) {
+			this.style = style;
+		}
+
+		@Override
+		public String toString() {
+			return "PoemOutput{" +
+					"title='" + title + '\'' +
+					", content='" + content + '\'' +
+					", style='" + style + '\'' +
+					'}';
+		}
+	}
+
+	// Inner class for BeanOutputConverter example
+	public static class ActorsFilms {
+		private String actor;
+		private List<String> films;
+
+		public String getActor() {
+			return actor;
+		}
+
+		public void setActor(String actor) {
+			this.actor = actor;
+		}
+
+		public List<String> getFilms() {
+			return films;
+		}
+
+		public void setFilms(List<String> films) {
+			this.films = films;
+		}
 	}
 
 }
