32e2f44188b2982c8191b96f1a002b72ed244901
chickenlj <ken.lj.hz@gmail.com>
2025-11-30 20:44:29 +0800

    update license
    Change-Id: I54370f67cc0a93520ed2023093e9cbb965c38162

--- spring-ai-alibaba-agent-framework/src/main/java/com/alibaba/cloud/ai/graph/agent/Prioritized.java
+++ spring-ai-alibaba-agent-framework/src/main/java/com/alibaba/cloud/ai/graph/agent/Prioritized.java
@@ -1,12 +1,11 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
+ * Copyright 2024-2025 the original author or authors.
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -25,7 +24,6 @@ package com.alibaba.cloud.ai.graph.agent;
  * to execute in a specific sequence.
  * </p>
  *
- * @author Spring AI Alibaba
  * @since 1.0.0
  */
 public interface Prioritized {
