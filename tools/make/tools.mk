8fb5e80d8b574175d31ab7b7faf07e6141680903
Wangzy <wangziyuan455@Gmail.com>
2025-12-31 21:41:38 +0800

    fix(ci): remove mvnd

--- tools/make/tools.mk
+++ tools/make/tools.mk
@@ -66,7 +66,7 @@ tools: ## Install ci tools
 	fi
 
 	@echo "Installing gitleaks"
-@if command -v gitleaks >/dev/null 2>&1; then \
+    @if command -v gitleaks >/dev/null 2>&1; then \
 		echo "gitleaks is already installed, skipping..."; \
 	else \
 		mkdir -p tools/bin && \
