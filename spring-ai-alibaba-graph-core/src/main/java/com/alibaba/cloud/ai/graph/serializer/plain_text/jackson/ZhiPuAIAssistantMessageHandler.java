5ab49ec2caae0239429e9f4c6f49d437ecf222ef
Yukina <rainyu@apache.org>
2026-02-09 21:06:51 +0800

    feat: support ZhiPu AI (#4225)

---
+++ spring-ai-alibaba-graph-core/src/main/java/com/alibaba/cloud/ai/graph/serializer/plain_text/jackson/ZhiPuAIAssistantMessageHandler.java
@@ -0,0 +1,149 @@
+/*
+ * Copyright 2024-2026 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.alibaba.cloud.ai.graph.serializer.plain_text.jackson;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonToken;
+import com.fasterxml.jackson.core.type.WritableTypeId;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
+import com.fasterxml.jackson.databind.jsontype.TypeSerializer;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+import org.springframework.ai.chat.messages.AssistantMessage;
+import org.springframework.ai.zhipuai.ZhiPuAiAssistantMessage;
+
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import static com.alibaba.cloud.ai.graph.serializer.plain_text.jackson.SerializationHelper.deserializeMetadata;
+import static com.alibaba.cloud.ai.graph.serializer.plain_text.jackson.SerializationHelper.serializeMetadata;
+
+public interface ZhiPuAIAssistantMessageHandler {
+
+	enum Field {
+
+		TEXT("text"), TOOL_CALLS("toolCalls"), REASONING_CONTENT("reasoningContent"), MEDIA("media");
+
+		final String name;
+
+		Field(String name) {
+			this.name = name;
+		}
+
+	}
+
+	class Serializer extends StdSerializer<ZhiPuAiAssistantMessage> {
+
+		public Serializer() {
+			super(ZhiPuAiAssistantMessage.class);
+		}
+
+		@Override
+		public void serialize(ZhiPuAiAssistantMessage msg, JsonGenerator gen, SerializerProvider provider)
+				throws IOException {
+			gen.writeStartObject();
+			serializeFields(msg, gen, provider);
+			gen.writeEndObject();
+		}
+
+		@Override
+		public void serializeWithType(ZhiPuAiAssistantMessage msg, JsonGenerator gen, SerializerProvider provider, TypeSerializer typeSer)
+				throws IOException {
+			WritableTypeId typeIdDef = typeSer.writeTypePrefix(gen, typeSer.typeId(msg, JsonToken.START_OBJECT));
+			serializeFields(msg, gen, provider);
+			typeSer.writeTypeSuffix(gen, typeIdDef);
+		}
+
+		private void serializeFields(ZhiPuAiAssistantMessage msg, JsonGenerator gen, SerializerProvider provider) throws IOException {
+			String text = msg.getText();
+			gen.writeStringField(Field.TEXT.name, text);
+
+			List<AssistantMessage.ToolCall> toolCalls = msg.getToolCalls();
+
+			gen.writeArrayFieldStart(Field.TOOL_CALLS.name);
+			for (var toolCall : toolCalls) {
+				gen.writeStartObject();
+				gen.writeStringField("id", toolCall.id());
+				gen.writeStringField("name", toolCall.name());
+				gen.writeStringField("type", toolCall.type());
+				gen.writeStringField("arguments", toolCall.arguments());
+				gen.writeEndObject();
+			}
+			gen.writeEndArray();
+
+			String reasoningContent = msg.getReasoningContent();
+
+			if (reasoningContent != null) {
+				gen.writeStringField(Field.REASONING_CONTENT.name, reasoningContent);
+			}
+			else {
+				gen.writeNullField(Field.REASONING_CONTENT.name);
+			}
+
+			Map<String, Object> metadata = msg.getMetadata();
+			serializeMetadata(gen, metadata);
+		}
+
+	}
+
+	class Deserializer extends StdDeserializer<ZhiPuAiAssistantMessage> {
+
+		protected Deserializer() {
+			super(ZhiPuAiAssistantMessage.class);
+		}
+
+		@Override
+		public ZhiPuAiAssistantMessage deserialize(JsonParser jsonParser, DeserializationContext ctx)
+				throws IOException {
+			var mapper = (ObjectMapper) jsonParser.getCodec();
+			ObjectNode node = mapper.readTree(jsonParser);
+
+			var textNode = node.get(Field.TEXT.name);
+			var text = (textNode != null && !textNode.isNull()) ? textNode.asText() : "";
+			var metadata = deserializeMetadata(mapper, node);
+			var requestsNode = node.get(Field.TOOL_CALLS.name);
+
+			var reasoningContentNode = node.get(Field.REASONING_CONTENT.name);
+			var reasoningContent = (reasoningContentNode != null && !reasoningContentNode.isNull())
+					? reasoningContentNode.asText() : null;
+
+			var requests = new LinkedList<AssistantMessage.ToolCall>();
+
+			if (requestsNode != null && !requestsNode.isNull() && !requestsNode.isEmpty()) {
+				for (JsonNode requestNode : requestsNode) {
+					var request = mapper.treeToValue(requestNode, AssistantMessage.ToolCall.class);
+					requests.add(request);
+				}
+			}
+
+			return new ZhiPuAiAssistantMessage.Builder()
+					.content(text)
+					.reasoningContent(reasoningContent)
+					.properties(metadata)
+					.toolCalls(requests)
+					.build();
+		}
+
+	}
+
+}
